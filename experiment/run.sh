033904caa18e3734a1816eeff22fa297405d72c7
Luigi Asprino <luigi.asprino@gmail.com>
2020-11-29 19:17:33 +0100

    sparql anything queries

--- experiment/run.sh
+++ experiment/run.sh
@@ -17,14 +17,27 @@ java -jar bin/sparql-generate-2.0.1.jar -q sparql-generate-queries/q5.rqg -l INF
 java -jar bin/sparql-generate-2.0.1.jar -q sparql-generate-queries/q6.rqg -l INFO
 java -jar bin/sparql-generate-2.0.1.jar -q sparql-generate-queries/q7.rqg -l INFO
 #java -jar bin/sparql-generate-2.0.1.jar -q sparql-generate-queries/q8.rqg -l INFO
-java -jar bin/sparql-generate-2.0.1.jar -q sparql-generate-queries/q9.rqg -fo TTL -o generated-data/q9.ttl -l INFO
-java -jar bin/sparql-generate-2.0.1.jar -q sparql-generate-queries/q10.rqg -fo TTL -o generated-data/q10.ttl -l INFO
-java -jar bin/sparql-generate-2.0.1.jar -q sparql-generate-queries/q11.rqg -fo TTL -o generated-data/q11.ttl -l INFO
-java -jar bin/sparql-generate-2.0.1.jar -q sparql-generate-queries/q12.rqg -fo TTL -o generated-data/q12.ttl -l INFO
+java -jar bin/sparql-generate-2.0.1.jar -q sparql-generate-queries/q9.rqg -fo TTL -o generated-data/sparql-generate-q9.ttl -l INFO
+java -jar bin/sparql-generate-2.0.1.jar -q sparql-generate-queries/q10.rqg -fo TTL -o generated-data/sparql-generate-q10.ttl -l INFO
+java -jar bin/sparql-generate-2.0.1.jar -q sparql-generate-queries/q11.rqg -fo TTL -o generated-data/sparql-generate-q11.ttl -l INFO
+java -jar bin/sparql-generate-2.0.1.jar -q sparql-generate-queries/q12.rqg -fo TTL -o generated-data/sparql-generate-q12.ttl -l INFO
 #java -jar bin/sparql-generate-2.0.1.jar -q sparql-generate-queries/test-aggregates.rqg -l INFO
 
 
-java -jar bin/rmlmapper.jar -m rml-mappings/m1.ttl -s turtle -o generated-data/m1.ttl
-java -jar bin/rmlmapper.jar -m rml-mappings/m2.ttl -s turtle -o generated-data/m2.ttl
-java -jar bin/rmlmapper.jar -m rml-mappings/m3.ttl -s turtle -o generated-data/m3.ttl
-java -jar bin/rmlmapper.jar -m rml-mappings/m4.ttl -s turtle -o generated-data/m4.ttl
+java -jar bin/rmlmapper.jar -m rml-mappings/m1.ttl -s turtle -o generated-data/rml-m1.ttl
+java -jar bin/rmlmapper.jar -m rml-mappings/m2.ttl -s turtle -o generated-data/rml-m2.ttl
+java -jar bin/rmlmapper.jar -m rml-mappings/m3.ttl -s turtle -o generated-data/rml-m3.ttl
+java -jar bin/rmlmapper.jar -m rml-mappings/m4.ttl -s turtle -o generated-data/rml-m4.ttl
+
+mvn exec:java  -f ../sparql.anything.engine/pom.xml -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="com.github.spiceh2020.sparql.anything.engine.SPARQLAnything" -Dexec.args="-q sparql-anything-queries/q1.rqg"
+mvn exec:java  -f ../sparql.anything.engine/pom.xml -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="com.github.spiceh2020.sparql.anything.engine.SPARQLAnything" -Dexec.args="-q sparql-anything-queries/q2.rqg"
+mvn exec:java  -f ../sparql.anything.engine/pom.xml -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="com.github.spiceh2020.sparql.anything.engine.SPARQLAnything" -Dexec.args="-q sparql-anything-queries/q3.rqg"
+mvn exec:java  -f ../sparql.anything.engine/pom.xml -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="com.github.spiceh2020.sparql.anything.engine.SPARQLAnything" -Dexec.args="-q sparql-anything-queries/q4.rqg"
+mvn exec:java  -f ../sparql.anything.engine/pom.xml -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="com.github.spiceh2020.sparql.anything.engine.SPARQLAnything" -Dexec.args="-q sparql-anything-queries/q5.rqg"
+mvn exec:java  -f ../sparql.anything.engine/pom.xml -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="com.github.spiceh2020.sparql.anything.engine.SPARQLAnything" -Dexec.args="-q sparql-anything-queries/q6.rqg"
+mvn exec:java  -f ../sparql.anything.engine/pom.xml -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="com.github.spiceh2020.sparql.anything.engine.SPARQLAnything" -Dexec.args="-q sparql-anything-queries/q7.rqg"
+mvn exec:java  -f ../sparql.anything.engine/pom.xml -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="com.github.spiceh2020.sparql.anything.engine.SPARQLAnything" -Dexec.args="-q sparql-anything-queries/q8.rqg"
+mvn exec:java  -f ../sparql.anything.engine/pom.xml -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="com.github.spiceh2020.sparql.anything.engine.SPARQLAnything" -Dexec.args="-q sparql-anything-queries/q9.rqg -f TTL -o generated-data/sparql-anything-q9.ttl"
+mvn exec:java  -f ../sparql.anything.engine/pom.xml -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="com.github.spiceh2020.sparql.anything.engine.SPARQLAnything" -Dexec.args="-q sparql-anything-queries/q10.rqg -f TTL -o generated-data/sparql-anything-q10.ttl"
+mvn exec:java  -f ../sparql.anything.engine/pom.xml -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="com.github.spiceh2020.sparql.anything.engine.SPARQLAnything" -Dexec.args="-q sparql-anything-queries/q11.rqg -f TTL -o generated-data/sparql-anything-q11.ttl"
+mvn exec:java  -f ../sparql.anything.engine/pom.xml -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="com.github.spiceh2020.sparql.anything.engine.SPARQLAnything" -Dexec.args="-q sparql-anything-queries/q12.rqg -f TTL -o generated-data/sparql-anything-q12.ttl"  
--- sparql.anything.engine/src/main/java/com/github/spiceh2020/sparql/anything/engine/SPARQLAnything.java
+++ sparql.anything.engine/src/main/java/com/github/spiceh2020/sparql/anything/engine/SPARQLAnything.java
@@ -2,8 +2,10 @@ package com.github.spiceh2020.sparql.anything.engine;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintStream;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -19,6 +21,7 @@ import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
 import org.apache.jena.query.ResultSetFormatter;
+import org.apache.jena.riot.Lang;
 import org.apache.jena.sparql.engine.ExecutionContext;
 import org.apache.jena.sparql.engine.main.OpExecutor;
 import org.apache.jena.sparql.engine.main.OpExecutorFactory;
@@ -33,6 +36,12 @@ public class SPARQLAnything {
 	private static final String QUERY = "q";
 	private static final String QUERY_LONG = "query";
 
+	private static final String OUTPUT = "o";
+	private static final String OUTPUT_LONG = "output";
+
+	private static final String FORMAT = "f";
+	private static final String FORMAT_LONG = "format";
+
 	private static final Logger logger = LogManager.getLogger(SPARQLAnything.class);
 
 	private static String getQuery(String queryArgument) throws IOException {
@@ -46,6 +55,7 @@ public class SPARQLAnything {
 			String line;
 			while ((line = br.readLine()) != null) {
 				sb.append(line);
+				sb.append('\n');
 			}
 			query = sb.toString();
 			br.close();
@@ -65,45 +75,70 @@ public class SPARQLAnything {
 		TriplifierRegister.getInstance().registerTriplifier(new JSONTriplifier());
 	}
 
-	private static void executeQuery(String query) {
-		logger.trace("Query: " + query);
+	private static void executeQuery(String query, PrintStream pw, String format) {
+		logger.trace("Executing Query: " + query);
 		Dataset kb = DatasetFactory.createGeneral();
 		Query q = QueryFactory.create(query);
 		if (q.isSelectType()) {
-			System.out.println(ResultSetFormatter.asText(QueryExecutionFactory.create(q, kb).execSelect()));
+			pw.println(ResultSetFormatter.asText(QueryExecutionFactory.create(q, kb).execSelect()));
 		} else if (q.isConstructType()) {
-			// TODO
+			QueryExecutionFactory.create(q, kb).execConstruct().write(pw, format);
 		} else if (q.isAskType()) {
-			// TODO
+			pw.println(QueryExecutionFactory.create(q, kb).execAsk());
 		} else if (q.isDescribeType()) {
-			// TODO
+			QueryExecutionFactory.create(q, kb).execDescribe().write(pw, format);
 		}
+	}
+
+	private static PrintStream getPrintWriter(CommandLine commandLine) throws FileNotFoundException {
+
+		if (commandLine.hasOption(FORMAT) && commandLine.hasOption(OUTPUT)) {
+			return new PrintStream(new File(commandLine.getOptionValue(OUTPUT)));
+		}
+
+		return System.out;
+	}
+
+	private static String getFormat(CommandLine commandLine) throws FileNotFoundException {
+
+		if (commandLine.hasOption(FORMAT)) {
+			return commandLine.getOptionValue(FORMAT);
+		}
+
+		return Lang.RDFXML.toString();
 	}
 
 	public static void main(String[] args) throws Exception {
 		logger.info("SPARQL anything");
 		Options options = new Options();
-		Option outputFileOption = Option.builder(QUERY).argName("query").hasArg().required(true)
-				.desc("The path to the file storing the query to execute or the query itself.").longOpt(QUERY_LONG)
-				.build();
 
-		options.addOption(outputFileOption);
+		options.addOption(Option.builder(QUERY).argName("query").hasArg().required(true)
+				.desc("The path to the file storing the query to execute or the query itself.").longOpt(QUERY_LONG)
+				.build());
+
+		options.addOption(Option.builder(OUTPUT).argName("file").hasArg()
+				.desc("OPTIONAL - The path to the output file. [Default: STDOUT]").longOpt(OUTPUT_LONG).build());
+
+		options.addOption(Option.builder(FORMAT).argName("string").hasArg()
+				.desc("OPTIONAL -  Format of the output file;  TTL, NT for construct queries [Default: TTL]")
+				.longOpt(FORMAT_LONG).build());
 
 		CommandLine commandLine = null;
 
 		CommandLineParser cmdLineParser = new DefaultParser();
 		try {
 			commandLine = cmdLineParser.parse(options, args);
+
+			String query = getQuery(commandLine.getOptionValue(QUERY));
+
+			initSPARQLAnythingEngine();
+
+			executeQuery(query, getPrintWriter(commandLine), getFormat(commandLine));
+
 		} catch (ParseException e) {
 			HelpFormatter formatter = new HelpFormatter();
 			formatter.printHelp("process", options);
 		}
-
-		String query = getQuery(commandLine.getOptionValue(QUERY));
-
-		initSPARQLAnythingEngine();
-
-		executeQuery(query);
 
 	}
 }
