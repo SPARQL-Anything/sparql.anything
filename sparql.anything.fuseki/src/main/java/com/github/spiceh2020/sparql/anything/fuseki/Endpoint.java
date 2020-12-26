031e3f7e3cfc373a49fd066b113553484cec655c
Luigi Asprino <luigi.asprino@gmail.com>
2020-12-26 10:06:00 +0100

    YASGUI added as Fuseki GUI

--- sparql.anything.fuseki/src/main/java/com/github/spiceh2020/sparql/anything/fuseki/Endpoint.java
+++ sparql.anything.fuseki/src/main/java/com/github/spiceh2020/sparql/anything/fuseki/Endpoint.java
@@ -17,23 +17,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.github.spiceh2020.sparql.anything.engine.FacadeX;
+import com.github.spiceh2020.sparql.anything.engine.FacadeXOpExecutor;
 
 public class Endpoint {
 
+	public static final String DEFAULT_PATH = "/sparql.anything";
+	public static final String DEFAULT_SPARQL_ENDPOINT_GUI_PATH = "/sparql";
+	public static final int DEFAULT_PORT = 3000;
+
 	private static Logger logger = LoggerFactory.getLogger(Endpoint.class);
 	private Builder builder = FusekiServer.create();
-	public static final String DEFAULT_PATH = "/sparql.anything";
-	private String path;
+	private String path, guipath;
 	private FusekiServer server;
 	private int port;
 	private static Endpoint instance;
-	public static final int DEFAULT_PORT = 3000;
-	private static final String PORT = "p", PATH = "e";
+	private static final String PORT = "p", PATH = "e", GUI = "g";
 
 	private Endpoint() {
 		builder.port(DEFAULT_PORT);
 		path = DEFAULT_PATH;
 		port = DEFAULT_PORT;
+		guipath = DEFAULT_SPARQL_ENDPOINT_GUI_PATH;
 	}
 
 	public static Endpoint getInstance() {
@@ -56,11 +60,22 @@ public class Endpoint {
 		}
 	}
 
+	public void setGUIPath(String guipath) {
+		if (path.charAt(0) != '/') {
+			this.guipath = "/" + guipath;
+		} else {
+			this.guipath = guipath;
+		}
+	}
+
 	public void start() {
 		Dataset ds = DatasetFactory.create();
 		QC.setFactory(ARQ.getContext(), FacadeX.ExecutorFactory);
 		logger.info("Starting sparql.anything endpoint..");
 		logger.info("The server will be listening on http://localhost:{}{}", port, path);
+		logger.info("The server will be available on http://localhost:{}{}", port, guipath);
+//		builder.staticFileBase("src/main/resources/static/");
+		builder.addServlet(this.guipath, new YASGUIServlet(FacadeXOpExecutor.FACADE_X_NAMESPACE_IRI, path));
 		server = builder.add(path, ds).build();
 		server.start();
 	}
@@ -83,6 +98,10 @@ public class Endpoint {
 				.desc("The path where the server will be running on (Default " + DEFAULT_PATH + ").").longOpt("path")
 				.build());
 
+		options.addOption(Option.builder(GUI).argName("gui").hasArg().required(false)
+				.desc("The path of the SPARQL endpoint GUI (Default " + DEFAULT_SPARQL_ENDPOINT_GUI_PATH + ").")
+				.longOpt("gui").build());
+
 		CommandLine commandLine = null;
 
 		CommandLineParser cmdLineParser = new DefaultParser();
@@ -90,11 +109,13 @@ public class Endpoint {
 			commandLine = cmdLineParser.parse(options, args);
 
 			String path = commandLine.getOptionValue(PATH, DEFAULT_PATH);
+			String guipath = commandLine.getOptionValue(GUI, DEFAULT_SPARQL_ENDPOINT_GUI_PATH);
 			int port = Integer.parseInt(commandLine.getOptionValue(PORT, DEFAULT_PORT + ""));
 
 			Endpoint e = Endpoint.getInstance();
 			e.setPath(path);
 			e.setPort(port);
+			e.setGUIPath(guipath);
 
 			e.start();
 
