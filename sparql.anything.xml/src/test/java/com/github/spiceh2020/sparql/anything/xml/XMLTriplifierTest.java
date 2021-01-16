e5083860d4ac25f18e3716ca9547e5d2748e6882
enridaga <enricodaga@gmail.com>
2021-01-16 19:05:51 +0000

    Changed test, now about #16

--- sparql.anything.xml/src/test/java/com/github/spiceh2020/sparql/anything/xml/XMLTriplifierTest.java
+++ sparql.anything.xml/src/test/java/com/github/spiceh2020/sparql/anything/xml/XMLTriplifierTest.java
@@ -2,10 +2,14 @@
 package com.github.spiceh2020.sparql.anything.xml;
 
 
+import com.github.spiceh2020.sparql.anything.model.Triplifier;
 import org.apache.jena.graph.Graph;
+import org.apache.jena.graph.NodeFactory;
 import org.apache.jena.graph.Triple;
 import org.apache.jena.sparql.core.DatasetGraph;
 import org.apache.jena.sparql.core.Quad;
+import org.apache.jena.vocabulary.RDF;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -20,13 +24,9 @@ public class XMLTriplifierTest {
     public void test1() throws IOException {
         Properties properties = new Properties();
         properties.setProperty("baseNamespace", "http://www.example.org#");
-//        properties.setProperty("uriRoot", "http://www.example.org#");
         URL xml1 = getClass().getClassLoader().getResource("./test1.xml");
         DatasetGraph graph = triplifier.triplify(xml1, properties);
-        Iterator<Quad> iter = graph.find(null,null,null,null);
-        while(iter.hasNext()){
-            Quad t = iter.next();
-            System.err.println(t);
-        }
+        Iterator<Quad> iter = graph.find(null,null, RDF.type.asNode(), NodeFactory.createURI(Triplifier.FACADE_X_TYPE_ROOT));
+        Assert.assertTrue(iter.hasNext());
     }
 }
