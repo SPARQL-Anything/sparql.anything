17e049d1467f36c8efeae7dd886b329a3157e823
Luigi Asprino <luigi.asprino@gmail.com>
2021-03-01 19:31:54 +0100

    Imported JSON2RDF code Added root type on JSON triplifier See #5

--- sparql.anything.json/src/main/java/com/github/spiceh2020/sparql/anything/json/JSONTriplifier.java
+++ sparql.anything.json/src/main/java/com/github/spiceh2020/sparql/anything/json/JSONTriplifier.java
@@ -1,6 +1,8 @@
 package com.github.spiceh2020.sparql.anything.json;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.Properties;
 import java.util.Set;
@@ -9,48 +11,183 @@ import org.apache.jena.ext.com.google.common.collect.Sets;
 import org.apache.jena.graph.NodeFactory;
 import org.apache.jena.query.DatasetFactory;
 import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.sparql.core.DatasetGraph;
-
-import com.github.spiceh2020.json2rdf.transformers.JSONTransformer;
-import com.github.spiceh2020.sparql.anything.model.IRIArgument;
-import com.github.spiceh2020.sparql.anything.model.Triplifier;
+import org.apache.jena.vocabulary.RDF;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.github.spiceh2020.sparql.anything.model.IRIArgument;
+import com.github.spiceh2020.sparql.anything.model.Triplifier;
 
 public class JSONTriplifier implements Triplifier {
 
 	private static Logger logger = LoggerFactory.getLogger(JSONTriplifier.class);
 
+	private String propertyPrefix, uriRoot;
+	private boolean useBlankNodes = true;
+
+//	private Model transformJSONFile(File input) throws IOException {
+//		BufferedReader br = new BufferedReader(new FileReader(input));
+//		StringBuilder sb = new StringBuilder();
+//		br.lines().forEachOrdered(l -> sb.append(l));
+//		br.close();
+//		return transformJSON(br.toString());
+//	}
+
+	private Model transformJSONFromURL(URL url) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+
+		if (propertyPrefix == null) {
+			propertyPrefix = url.toString() + "/";
+		}
+		StringBuilder sb = new StringBuilder();
+		br.lines().forEachOrdered(l -> {
+			sb.append(l);
+			sb.append('\n');
+		});
+		br.close();
+
+		return transformJSON(sb.toString());
+	}
+
+	private void reset() {
+		propertyPrefix = null;
+		uriRoot = null;
+		useBlankNodes = true;
+	}
+
+	private Model transformJSON(String json) {
+		checkParameters();
+		try {
+			return getModel(new JSONObject(json));
+		} catch (JSONException e) {
+			return getModel(new JSONArray(json));
+		}
+	}
+
+	private void checkParameters() {
+		if (propertyPrefix == null)
+			throw new RuntimeException("The property prefix can't be null");
+	}
+
+	public Model getModel(JSONObject object) {
+		Model m = ModelFactory.createDefaultModel();
+		Resource root = createResource(uriRoot);
+		m.add(root, RDF.type, m.createResource(Triplifier.FACADE_X_TYPE_ROOT));
+		getModel(object, root, m);
+		return m;
+	}
+
+	public Model getModel(JSONArray arr) {
+		Model m = ModelFactory.createDefaultModel();
+		Resource root = createResource(uriRoot);
+		m.add(root, RDF.type, m.createResource(Triplifier.FACADE_X_TYPE_ROOT));
+		getModel(arr,root, m);
+		return m;
+	}
+
+	private void getModel(JSONObject object, Resource r, Model m) {
+//		m.add(r, RDF.type, RDFS.Resource);
+		object.keys().forEachRemaining(k -> {
+			Object o = object.get(k);
+			Property p = m.createProperty(propertyPrefix + k);
+//			m.add(p, RDFS.label, m.createTypedLiteral(k));
+			if (o instanceof String || o instanceof Boolean || o instanceof Integer) {
+				transformPrimites(m, r, p, o);
+			} else if (o instanceof JSONObject) {
+				transformJSONObject(m, r, p, (JSONObject) o);
+			} else if (o instanceof JSONArray) {
+				transformArray(m, r, p, (JSONArray) o);
+			}
+		});
+	}
+
+	private void getModel(JSONArray arr, Resource r, Model m) {
+//		m.add(r, RDF.type, RDF.Seq);
+		for (int i = 0; i < arr.length(); i++) {
+			Object o = arr.get(i);
+			Property p = RDF.li(i + 1);
+			if (o instanceof String || o instanceof Boolean || o instanceof Integer) {
+				transformPrimites(m, r, p, o);
+			} else if (o instanceof JSONObject) {
+				transformJSONObject(m, r, p, (JSONObject) o);
+			} else if (o instanceof JSONArray) {
+				transformArray(m, r, p, (JSONArray) o);
+			}
+		}
+		;
+	}
+
+	private void transformArray(Model m, Resource r, Property p, JSONArray o) {
+		Resource seq = createResource(r.getURI() + "/" + p.getLocalName());
+		m.add(r, p, seq);
+		getModel(o, seq, m);
+	}
+
+	private void transformJSONObject(Model m, Resource r, Property p, JSONObject o) {
+		Resource rnew = createResource(r.getURI() + "/" + p.getLocalName());
+		m.add(r, p, rnew);
+		getModel(o, rnew, m);
+	}
+
+	private void transformPrimites(Model m, Resource r, Property p, Object o) {
+		m.add(r, p, m.createTypedLiteral(o));
+	}
+
+	private Resource createResource(String path) {
+		if (useBlankNodes) {
+			return ModelFactory.createDefaultModel().createResource();
+		} else {
+			return ModelFactory.createDefaultModel().createResource(path);
+		}
+
+	}
+
+	public void setPropertyPrefix(String propertyPrefix) {
+		this.propertyPrefix = propertyPrefix;
+	}
+
+	public void setURIRoot(String uri) {
+		if (uri != null) {
+			this.uriRoot = uri;
+			useBlankNodes = false;
+		}
+	}
+
 	@Override
 	public DatasetGraph triplify(URL url, Properties properties) throws IOException {
 		logger.trace("Triplifying " + url.toString());
-		JSONTransformer jt;
+		
+		// FIXME It would be better to generalise control of arguments
 		if (properties.containsKey(IRIArgument.NAMESPACE.toString())) {
 			logger.trace("Property prefix provided");
-			jt = new JSONTransformer(properties.getProperty(IRIArgument.NAMESPACE.toString()));
+			propertyPrefix = properties.getProperty(IRIArgument.NAMESPACE.toString());
 		} else {
 			logger.trace("Property prefix not provided");
-			jt = new JSONTransformer(url.toString() + "/");
+			propertyPrefix = properties.getProperty(url.toString() + "/");
 		}
-
-		boolean blank_nodes = true;
 		if (properties.containsKey(IRIArgument.BLANK_NODES.toString())) {
-			blank_nodes = Boolean.parseBoolean(properties.getProperty(IRIArgument.BLANK_NODES.toString()));
+			useBlankNodes = Boolean.parseBoolean(properties.getProperty(IRIArgument.BLANK_NODES.toString()));
 		}
 
 		String root = url.toString();
 		if (properties.containsKey(IRIArgument.ROOT.toString())) {
 			root = properties.getProperty(IRIArgument.ROOT.toString());
+			this.setURIRoot(root);
 		}
 
-		if (!blank_nodes) {
-			jt.setURIRoot(root);
-		}
-		// TODO Add triple _:root rdf:type Triplifier.FACADE_X_TYPE_ROOT
-		Model m = jt.transformJSONFromURL(url);
+		Model m = transformJSONFromURL(url);
 		logger.trace("Number of triples " + m.size());
 		DatasetGraph dg = DatasetFactory.create(m).asDatasetGraph();
 		dg.addGraph(NodeFactory.createURI(url.toString()), dg.getDefaultGraph());
+		// FIXME quick and dirty solution for resetting fields
+		reset();
 		return dg;
 	}
 
---
+++ sparql.anything.json/src/test/java/com/github/spiceh2020/sparql/anything/json/test/JSON2RDFTransformerTest.java
@@ -0,0 +1,196 @@
+package com.github.spiceh2020.sparql.anything.json.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.sparql.core.DatasetGraph;
+import org.apache.jena.vocabulary.RDF;
+import org.json.JSONException;
+import org.junit.Test;
+
+import com.github.spiceh2020.sparql.anything.json.JSONTriplifier;
+import com.github.spiceh2020.sparql.anything.model.IRIArgument;
+import com.github.spiceh2020.sparql.anything.model.Triplifier;
+
+public class JSON2RDFTransformerTest {
+
+	private String ontologyPrefix = "https://w3id.org/resource/ontology/";
+
+	@Test
+	public void testEmptyAndNull() {
+
+		JSONTriplifier jt = new JSONTriplifier();
+
+		try {
+			DatasetGraph g1 = jt.triplify(getClass().getClassLoader().getResource("./emptyobject.json"),
+					new Properties());
+			DatasetGraph g2 = jt.triplify(getClass().getClassLoader().getResource("./emptyarray.json"),
+					new Properties());
+			assertEquals(1L, g1.getDefaultGraph().size());
+			assertEquals(1L, g2.getDefaultGraph().size());
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
+		boolean jsonException = false;
+		try {
+			jt.triplify(getClass().getClassLoader().getResource("./emptyfile"), new Properties());
+		} catch (JSONException e) {
+			jsonException = true;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		assertTrue(jsonException);
+
+		boolean nullPointerException = false;
+		try {
+			jt.triplify(null, new Properties());
+		} catch (NullPointerException e) {
+			nullPointerException = true;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		assertTrue(nullPointerException);
+
+	}
+
+	@Test
+	public void testPrimitive() {
+		{
+
+			{
+				JSONTriplifier jt = new JSONTriplifier();
+				Properties properties = new Properties();
+				properties.setProperty(IRIArgument.NAMESPACE.toString(), ontologyPrefix);
+				Model m = ModelFactory.createDefaultModel();
+				Resource r = m.createResource();
+				m.add(r,RDF.type, m.createResource(Triplifier.FACADE_X_TYPE_ROOT));
+				m.add(r, m.createProperty(ontologyPrefix + "a"), m.createTypedLiteral(1));
+				m.add(r, m.createProperty(ontologyPrefix + "string"), m.createTypedLiteral("string"));
+				m.add(r, m.createProperty(ontologyPrefix + "bool"), m.createTypedLiteral(true));
+				m.add(r, m.createProperty(ontologyPrefix + "boolf"), m.createTypedLiteral(false));
+
+				DatasetGraph g1;
+				try {
+					g1 = jt.triplify(getClass().getClassLoader().getResource("./testprimitive.json"), properties);
+					assertTrue(m.getGraph().isIsomorphicWith(g1.getDefaultGraph()));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+			{
+				JSONTriplifier jt = new JSONTriplifier();
+				Properties properties = new Properties();
+				String root = "https://w3id.org/spice/resource/root";
+				properties.setProperty(IRIArgument.NAMESPACE.toString(), ontologyPrefix);
+				properties.setProperty(IRIArgument.ROOT.toString(), root);
+
+				Model mn = ModelFactory.createDefaultModel();
+				Resource rn = mn.createResource(root);
+				mn.add(rn,RDF.type, mn.createResource(Triplifier.FACADE_X_TYPE_ROOT));
+				mn.add(rn, mn.createProperty(ontologyPrefix + "a"), mn.createTypedLiteral(1));
+				mn.add(rn, mn.createProperty(ontologyPrefix + "string"), mn.createTypedLiteral("string"));
+				mn.add(rn, mn.createProperty(ontologyPrefix + "bool"), mn.createTypedLiteral(true));
+				mn.add(rn, mn.createProperty(ontologyPrefix + "boolf"), mn.createTypedLiteral(false));
+
+				DatasetGraph g1;
+				try {
+					g1 = jt.triplify(getClass().getClassLoader().getResource("./testprimitive.json"), properties);
+					ModelFactory.createModelForGraph(g1.getDefaultGraph()).write(System.out, "TTL");
+					assertTrue(mn.getGraph().isIsomorphicWith(g1.getDefaultGraph()));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+
+	@Test
+	public void testArray() {
+		{
+			{
+				Model m = ModelFactory.createDefaultModel();
+				Resource r = m.createResource();
+				m.add(r,RDF.type, m.createResource(Triplifier.FACADE_X_TYPE_ROOT));
+				m.add(r, RDF.li(1), m.createTypedLiteral(1));
+				m.add(r, RDF.li(2), m.createTypedLiteral("abcd"));
+				Resource o = m.createResource();
+				m.add(r, RDF.li(3), o);
+				m.add(o, m.createProperty(ontologyPrefix + "a"), m.createTypedLiteral("a"));
+				m.add(r, RDF.li(5), m.createTypedLiteral(4));
+				Resource arr = m.createResource();
+				m.add(o, m.createProperty(ontologyPrefix + "arr"), arr);
+				m.add(arr, RDF.li(1), m.createTypedLiteral(0));
+				m.add(arr, RDF.li(2), m.createTypedLiteral(1));
+
+				Properties properties = new Properties();
+				properties.setProperty(IRIArgument.NAMESPACE.toString(), ontologyPrefix);
+				JSONTriplifier jt = new JSONTriplifier();
+
+				DatasetGraph g1;
+				try {
+					g1 = jt.triplify(getClass().getClassLoader().getResource("./testarray.json"), properties);
+					assertTrue(m.getGraph().isIsomorphicWith(g1.getDefaultGraph()));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+			}
+
+			{
+				String root = "https://w3id.org/spice/resource/root";
+				Model m = ModelFactory.createDefaultModel();
+				Resource r = m.createResource(root);
+				m.add(r,RDF.type, m.createResource(Triplifier.FACADE_X_TYPE_ROOT));
+				m.add(r, RDF.li(1), m.createTypedLiteral(1));
+				m.add(r, RDF.li(2), m.createTypedLiteral("abcd"));
+				Resource o = m.createResource(root + "/_3");
+				m.add(r, RDF.li(3), o);
+				m.add(o, m.createProperty(ontologyPrefix + "a"), m.createTypedLiteral("a"));
+				m.add(r, RDF.li(5), m.createTypedLiteral(4));
+				Resource arr = m.createResource(root + "/_3/arr");
+				m.add(o, m.createProperty(ontologyPrefix + "arr"), arr);
+				m.add(arr, RDF.li(1), m.createTypedLiteral(0));
+				m.add(arr, RDF.li(2), m.createTypedLiteral(1));
+				Properties properties = new Properties();
+
+				properties.setProperty(IRIArgument.NAMESPACE.toString(), ontologyPrefix);
+				properties.setProperty(IRIArgument.ROOT.toString(), root);
+				JSONTriplifier jt = new JSONTriplifier();
+
+				DatasetGraph g1;
+				try {
+					g1 = jt.triplify(getClass().getClassLoader().getResource("./testarray.json"), properties);
+					ModelFactory.createModelForGraph(g1.getDefaultGraph()).write(System.out, "TTL");
+					assertTrue(m.getGraph().isIsomorphicWith(g1.getDefaultGraph()));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+
+	@Test(expected = org.json.JSONException.class)
+	public void testDuplicateKeyJSON() {
+		{
+			JSONTriplifier jt = new JSONTriplifier();
+			try {
+				jt.triplify(getClass().getClassLoader().getResource("./testduplicatekey.json"), new Properties());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
+}
