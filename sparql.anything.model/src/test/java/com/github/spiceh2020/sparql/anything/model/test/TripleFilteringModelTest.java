f989dc73fd9d719388eb7a2a5df1ee441d8c7a52
enridaga <enricodaga@gmail.com>
2021-03-16 12:11:34 +0000

    Tests with QP. See #41

--- sparql.anything.model/src/test/java/com/github/spiceh2020/sparql/anything/model/test/TripleFilteringModelTest.java
+++ sparql.anything.model/src/test/java/com/github/spiceh2020/sparql/anything/model/test/TripleFilteringModelTest.java
@@ -30,6 +30,9 @@ import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdf.model.ResourceFactory;
 import org.apache.jena.sparql.algebra.op.OpBGP;
+import org.apache.jena.sparql.algebra.op.OpQuad;
+import org.apache.jena.sparql.algebra.op.OpQuadPattern;
+import org.apache.jena.sparql.core.Quad;
 import org.apache.jena.vocabulary.RDF;
 import org.junit.Assert;
 import org.junit.Test;
@@ -167,6 +170,63 @@ public class TripleFilteringModelTest {
         bgp.getPattern().add(new Triple(Node_Variable.ANY, new Node_Variable("p"), Node_Variable.ANY));
 
         TripleFilteringModel f = new TripleFilteringModel(NodeFactory.createURI("http://www.example.org/"), bgp);
+        //
+        f.add(ResourceFactory.createResource(), property1, ResourceFactory.createPlainLiteral("Hello world"));
+        Assert.assertTrue(f.getModel().size() == 1);
+        f.add(ResourceFactory.createResource(), property2, ResourceFactory.createPlainLiteral("Hello world"));
+        Assert.assertTrue(f.getModel().size() == 2);
+        //
+        f.add(resource1, property1, resource3); // OK
+        Assert.assertTrue(f.getModel().size() == 3);
+        f.add(resource1, property1, resource2); // OK
+        Assert.assertTrue(f.getModel().size() == 4);
+
+        f.add(resource2, property2, resource4); // OK
+        Assert.assertTrue(f.getModel().size() == 5);
+        f.add(resource2, property1, resource2); // OK
+        f.add(resource2, property1, resource3); // OK
+        f.add(resource2, property1, resource4); // OK
+        f.add(resource1, property2, resource4); // OK
+        Assert.assertTrue(f.getModel().size() == 9);
+    }
+
+    /**
+     * QP, 1, {? ? ? L}
+     */
+    @Test
+    public void qp1(){
+        OpBGP bgp = new OpBGP();
+        bgp.getPattern().add(new Triple(new Node_Variable("a"), new Node_Variable("b"), ResourceFactory.createPlainLiteral("Hello world").asNode()));
+
+        OpQuadPattern qp = new OpQuadPattern(new Node_Variable("g"), bgp.getPattern());
+
+        TripleFilteringModel f = new TripleFilteringModel(NodeFactory.createURI("http://www.example.org/"), qp);
+        f.add(ResourceFactory.createResource(), RDF.type, ResourceFactory.createPlainLiteral("Hello world"));
+        Assert.assertTrue(f.getModel().size() == 1);
+        f.add(ResourceFactory.createResource(), RDF.type, ResourceFactory.createPlainLiteral("Hello world not!"));
+        Assert.assertTrue(f.getModel().size() == 1);
+    }
+
+    /**
+     * QP, 1, {G1 {R1 P1 R3, R2 P2 R4, [] ? []}}
+     */
+    @Test
+    public void qp2(){
+        Property property1 = ResourceFactory.createProperty("http://sparql.xyz/facade-x/data/property1");
+        Property property2 = ResourceFactory.createProperty("http://sparql.xyz/facade-x/data/property2");
+        Resource resource1 = ResourceFactory.createResource("http://sparql.xyz/facade-x/data/resource1");
+        Resource resource2 = ResourceFactory.createResource("http://sparql.xyz/facade-x/data/resource2");
+        Resource resource3 = ResourceFactory.createResource("http://sparql.xyz/facade-x/data/resource3");
+        Resource resource4 = ResourceFactory.createResource("http://sparql.xyz/facade-x/data/resource4");
+
+        OpBGP bgp = new OpBGP();
+        bgp.getPattern().add(new Triple(resource1.asNode(), property1.asNode(), resource3.asNode()));
+        bgp.getPattern().add(new Triple(resource2.asNode(), property2.asNode(), resource4.asNode()));
+        bgp.getPattern().add(new Triple(Node_Variable.ANY, new Node_Variable("p"), Node_Variable.ANY));
+
+        OpQuadPattern qp = new OpQuadPattern(NodeFactory.createURI("http://www.example.org/"), bgp.getPattern());
+
+        TripleFilteringModel f = new TripleFilteringModel(NodeFactory.createURI("http://www.example.org/"), qp);
         //
         f.add(ResourceFactory.createResource(), property1, ResourceFactory.createPlainLiteral("Hello world"));
         Assert.assertTrue(f.getModel().size() == 1);
