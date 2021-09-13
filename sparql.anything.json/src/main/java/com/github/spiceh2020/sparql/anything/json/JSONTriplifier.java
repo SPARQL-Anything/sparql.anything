61efae4103198173e5ba735547f6330bf870b291
enridaga <enricodaga@gmail.com>
2021-09-13 16:08:42 +0100

    Refactored variable name filter > builder. See #91

--- sparql.anything.json/src/main/java/com/github/spiceh2020/sparql/anything/json/JSONTriplifier.java
+++ sparql.anything.json/src/main/java/com/github/spiceh2020/sparql/anything/json/JSONTriplifier.java
@@ -35,7 +35,7 @@ public class JSONTriplifier implements Triplifier {
 	private static final byte[] BUFF = new byte[1024];
 
 	private void transformJSONFromURL(URL url, String rootId, Properties properties,
-			FacadeXGraphBuilder filter) throws IOException {
+			FacadeXGraphBuilder builder) throws IOException {
 		JsonIterator.setMode(DecodingMode.DYNAMIC_MODE_AND_MATCH_FIELD_WITH_HASH);
 		JsonStream.setMode(EncodingMode.DYNAMIC_MODE);
 
@@ -53,7 +53,7 @@ public class JSONTriplifier implements Triplifier {
 				StandardCharsets.UTF_8);
 		try {
 			json.reset(stream);
-			transformJSON(json, url.toString(), rootId, filter);
+			transformJSON(json, url.toString(), rootId, builder);
 		} finally {
 			stream.close();
 			us.close();
@@ -62,10 +62,10 @@ public class JSONTriplifier implements Triplifier {
 
 //	private void any(String jsoon)
 
-	private void transformJSON(JsonIterator json, String dataSourceId, String rootId, FacadeXGraphBuilder filter)
+	private void transformJSON(JsonIterator json, String dataSourceId, String rootId, FacadeXGraphBuilder builder)
 			throws IOException {
 
-		filter.addRoot(dataSourceId, rootId);
+		builder.addRoot(dataSourceId, rootId);
 //		Any any = json.readAny();
 //
 //		if(any.valueType().equals(ValueType.OBJECT)) {
@@ -77,43 +77,43 @@ public class JSONTriplifier implements Triplifier {
 //		}
 		if (json.whatIsNext().equals(ValueType.OBJECT)) {
 			Map<String, Any> object = json.readAny().asMap();
-			transform(object, dataSourceId, rootId, filter);
+			transform(object, dataSourceId, rootId, builder);
 		} else {
 			int i = 0;
 			while (json.readArray()) {
-				transformArrayElement(json.readAny(), i, dataSourceId, rootId, filter);
+				transformArrayElement(json.readAny(), i, dataSourceId, rootId, builder);
 				i++;
 			}
 		}
 	}
 
 	private void transform(Map<String, Any> object, String dataSourceId, String containerId,
-			FacadeXGraphBuilder filter) {
+			FacadeXGraphBuilder builder) {
 		object.keySet().iterator().forEachRemaining(k -> {
 			Any o = object.get(k);
 			if (o.valueType().equals(ValueType.OBJECT) || o.valueType().equals(ValueType.ARRAY)) {
 				String childContainerId = StringUtils.join(containerId, "/", Triplifier.toSafeURIString(k));
-				filter.addContainer(dataSourceId, containerId, Triplifier.toSafeURIString(k), childContainerId);
+				builder.addContainer(dataSourceId, containerId, Triplifier.toSafeURIString(k), childContainerId);
 				if (o.valueType().equals(ValueType.OBJECT)) {
-					transform(o.asMap(), dataSourceId, childContainerId, filter);
+					transform(o.asMap(), dataSourceId, childContainerId, builder);
 				} else {
-					transform(o.asList(), dataSourceId, childContainerId, filter);
+					transform(o.asList(), dataSourceId, childContainerId, builder);
 				}
 			} else
 			// Value
 			if (((Any) o).valueType().equals(ValueType.STRING)) {
-				filter.addValue(dataSourceId, containerId, Triplifier.toSafeURIString(k), ((Any) o).toString());
+				builder.addValue(dataSourceId, containerId, Triplifier.toSafeURIString(k), ((Any) o).toString());
 			} else if (((Any) o).valueType().equals(ValueType.BOOLEAN)) {
-				filter.addValue(dataSourceId, containerId, Triplifier.toSafeURIString(k), ((Any) o).toBoolean());
+				builder.addValue(dataSourceId, containerId, Triplifier.toSafeURIString(k), ((Any) o).toBoolean());
 			} else if (((Any) o).valueType().equals(ValueType.NUMBER)) {
 //				filter.addValue(dataSourceId, containerId, Triplifier.toSafeURIString(k), ((Any) o).toString());
 				if (o.toString().contains(".")) {
-					filter.addValue(dataSourceId, containerId, Triplifier.toSafeURIString(k), ((Any) o).toFloat());
+					builder.addValue(dataSourceId, containerId, Triplifier.toSafeURIString(k), ((Any) o).toFloat());
 				} else if (o.toString().contains("0")) {
 					// There is a bug with Jsoniter when the int number contains a zero, we do the cast ourselves in that case
-					filter.addValue(dataSourceId, containerId, Triplifier.toSafeURIString(k), Integer.valueOf(o.toString().trim()));
+					builder.addValue(dataSourceId, containerId, Triplifier.toSafeURIString(k), Integer.valueOf(o.toString().trim()));
 				} else {
-					filter.addValue(dataSourceId, containerId, Triplifier.toSafeURIString(k), ((Any) o).toInt());
+					builder.addValue(dataSourceId, containerId, Triplifier.toSafeURIString(k), ((Any) o).toInt());
 				}
 			}
 		});
@@ -148,35 +148,35 @@ public class JSONTriplifier implements Triplifier {
 //		}
 //	}
 
-	private void transform(List<Any> arr, String dataSourceId, String containerId, FacadeXGraphBuilder filter) {
+	private void transform(List<Any> arr, String dataSourceId, String containerId, FacadeXGraphBuilder builder) {
 		for (int i = 0; i < arr.size(); i++) {
 			Any o = arr.get(i);
-			transformArrayElement(o, i, dataSourceId, containerId, filter);
+			transformArrayElement(o, i, dataSourceId, containerId, builder);
 		}
 	}
 
 	private void transformArrayElement(Any o, int i, String dataSourceId, String containerId,
-			FacadeXGraphBuilder filter) {
+			FacadeXGraphBuilder builder) {
 		if (o.valueType().equals(ValueType.OBJECT) || o.valueType().equals(ValueType.ARRAY)) {
 			String childContainerId = StringUtils.join(containerId, "/_", String.valueOf(i + 1));
-			filter.addContainer(dataSourceId, containerId, i + 1, childContainerId);
+			builder.addContainer(dataSourceId, containerId, i + 1, childContainerId);
 			if (o.valueType().equals(ValueType.OBJECT)) {
-				transform(o.asMap(), dataSourceId, childContainerId, filter);
+				transform(o.asMap(), dataSourceId, childContainerId, builder);
 			} else {
-				transform(o.asList(), dataSourceId, childContainerId, filter);
+				transform(o.asList(), dataSourceId, childContainerId, builder);
 			}
 		} else if (((Any) o).valueType().equals(ValueType.STRING)) {
-			filter.addValue(dataSourceId, containerId, i + 1, ((Any) o).toString());
+			builder.addValue(dataSourceId, containerId, i + 1, ((Any) o).toString());
 		} else if (((Any) o).valueType().equals(ValueType.BOOLEAN)) {
-			filter.addValue(dataSourceId, containerId, i + 1, ((Any) o).toBoolean());
+			builder.addValue(dataSourceId, containerId, i + 1, ((Any) o).toBoolean());
 		} else if (((Any) o).valueType().equals(ValueType.NUMBER)) {
 			log.info("Type: {} {} {}", o.valueType(), o.getClass(), o.toString());
 			if (o.toString().contains(".")) {
-				filter.addValue(dataSourceId, containerId, i + 1, ((Any) o).toFloat());
+				builder.addValue(dataSourceId, containerId, i + 1, ((Any) o).toFloat());
 			} else if (o.toString().equals("0")) {
-				filter.addValue(dataSourceId, containerId, i + 1, 0);
+				builder.addValue(dataSourceId, containerId, i + 1, 0);
 			} else {
-				filter.addValue(dataSourceId, containerId, i + 1, ((Any) o).toInt());
+				builder.addValue(dataSourceId, containerId, i + 1, ((Any) o).toInt());
 			}
 		}
 	}
