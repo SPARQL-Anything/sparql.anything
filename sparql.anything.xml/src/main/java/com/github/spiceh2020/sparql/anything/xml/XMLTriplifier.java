474e81c771b58bd54b1a57fe84f22265c5ec1737
enridaga <enricodaga@gmail.com>
2021-09-14 12:58:37 +0100

    Implemented graph builder in the XML triplifier. About #41 and #91

--- sparql.anything.xml/src/main/java/com/github/spiceh2020/sparql/anything/xml/XMLTriplifier.java
+++ sparql.anything.xml/src/main/java/com/github/spiceh2020/sparql/anything/xml/XMLTriplifier.java
@@ -2,6 +2,8 @@ package com.github.spiceh2020.sparql.anything.xml;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayDeque;
 import java.util.Deque;
@@ -21,13 +23,13 @@ import javax.xml.stream.events.XMLEvent;
 
 import com.github.spiceh2020.sparql.anything.model.FacadeXGraphBuilder;
 import org.apache.jena.ext.com.google.common.collect.Sets;
-import org.apache.jena.graph.NodeFactory;
+//import org.apache.jena.graph.NodeFactory;
 import org.apache.jena.query.DatasetFactory;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.Property;
-import org.apache.jena.rdf.model.Resource;
-import org.apache.jena.rdf.model.ResourceFactory;
+//import org.apache.jena.rdf.model.Model;
+//import org.apache.jena.rdf.model.ModelFactory;
+//import org.apache.jena.rdf.model.Property;
+//import org.apache.jena.rdf.model.Resource;
+//import org.apache.jena.rdf.model.ResourceFactory;
 import org.apache.jena.sparql.core.DatasetGraph;
 import org.apache.jena.sparql.core.DatasetGraphFactory;
 import org.apache.jena.vocabulary.RDF;
@@ -48,19 +50,19 @@ public class XMLTriplifier implements Triplifier {
 			return DatasetGraphFactory.create();
 
 		String namespace = Triplifier.getNamespaceArgument(properties);
-
+		String dataSourceId = url.toString();
 		String root = Triplifier.getRootArgument(properties, url);
 
 		boolean blank_nodes = Triplifier.getBlankNodeArgument(properties);
 
-		Model model = ModelFactory.createDefaultModel();
+//		Model model = ModelFactory.createDefaultModel();
 		//
 		XMLInputFactory inputFactory = XMLInputFactory.newInstance();
 		XMLEvent event = null;
 		XMLEventReader eventReader;
 		//
-		Deque<Resource> stack = new ArrayDeque<Resource>();
-		Map<Resource, Integer> members = new HashMap<Resource, Integer>();
+		Deque<String> stack = new ArrayDeque<String>();
+		Map<String, Integer> members = new HashMap<String, Integer>();
 		String path = "";
 		StringBuilder charBuilder = null;
 		//
@@ -84,12 +86,19 @@ public class XMLTriplifier implements Triplifier {
 					String value = charBuilder.toString();
 					log.trace("collecting char stream: {}", value);
 					if (stack.peekLast() != null && value != null && !value.trim().equals("")) {
-						Resource resource = stack.peekLast();
-						if (!members.containsKey(resource)) {
-							members.put(resource, 0);
+////						Resource resource = stack.peekLast();
+//						if (!members.containsKey(resource)) {
+//							members.put(resource, 0);
+//						}
+//						int member = members.get(resource) + 1;
+//						resource.addProperty(RDF.li(member), value);
+
+						String resourceId = stack.peekLast();
+						if (!members.containsKey(resourceId)) {
+							members.put(resourceId, 0);
 						}
-						int member = members.get(resource) + 1;
-						resource.addProperty(RDF.li(member), value);
+						int member = members.get(resourceId) + 1;
+						builder.addValue(dataSourceId, resourceId, member, value);
 					}
 					charBuilder = null;
 				}
@@ -113,7 +122,8 @@ public class XMLTriplifier implements Triplifier {
 				}
 				int member = 0;
 				if (stack.size() > 0) {
-					Resource parent = stack.peekLast();
+//					Resource parent = stack.peekLast();
+					String parent = stack.peekLast();
 					member = members.get(parent) + 1;
 					members.put(parent, member);
 				}
@@ -126,43 +136,56 @@ public class XMLTriplifier implements Triplifier {
 				log.trace("element open: {} [{}]", path, stack.size());
 
 				// XXX Create an RDF resource
-				Resource resource;
-				if (blank_nodes == false) {
-					if (isRoot) {
-						resource = model.createResource(root);
-					} else {
-//						resource = model.createResource(namespace + event.asStartElement().hashCode());
-						resource = model.createResource(String.join("", new String[] { namespace, path.substring(1) }));
-					}
-				} else {
-					resource = model.createResource();
-				}
+				String resourceId = path.substring(1);
+//				if (blank_nodes == false) {
+//					if (isRoot) {
+//						resource = model.createResource(root);
+//					} else {
+////						resource = model.createResource(namespace + event.asStartElement().hashCode());
+//						resource = model.createResource(String.join("", new String[] { namespace, path.substring(1) }));
+//					}
+//				} else {
+//					resource = model.createResource();
+//				}
 				// If this is the root
 				if (isRoot) {
 					// Add type root
-					resource.addProperty(RDF.type, ResourceFactory.createResource(Triplifier.FACADE_X_TYPE_ROOT));
+//					resource.addProperty(RDF.type, ResourceFactory.createResource(Triplifier.FACADE_X_TYPE_ROOT));
+					builder.addRoot(dataSourceId, resourceId);
 					isRoot = false;
 				}
 				// XXX Type is element name
-				resource.addProperty(RDF.type, ResourceFactory.createResource(toIRI(se.getName(), namespace)));
-				if (!members.containsKey(resource)) {
-					members.put(resource, 0);
+//				resource.addProperty(RDF.type, ResourceFactory.createResource(toIRI(se.getName(), namespace)));
+				try {
+					builder.addType(dataSourceId, resourceId, new URI(toIRI(se.getName(), namespace)));
+				} catch (URISyntaxException e) {
+					throw new IOException(e);
+				}
+				if (!members.containsKey(resourceId)) {
+					members.put(resourceId, 0);
 				}
 				// Link it with the container membership property
 				if (stack.size() > 0) {
-					Resource parent = stack.peekLast();
-					Property property = RDF.li(member);
-					parent.addProperty(property, resource);
+//					Resource parent = stack.peekLast();
+					String parent = stack.peekLast();
+					builder.addContainer(dataSourceId, parent, member, resourceId);
+//					Property property = RDF.li(member);
+//					parent.addProperty(property, resource);
 				}
 				// Attributes
 				Iterator<Attribute> attributes = se.getAttributes();
 				while (attributes.hasNext()) {
 					Attribute attribute = (Attribute) attributes.next();
 					log.trace("attribute: {}", attribute);
-					Property property = model.createProperty(toIRI(attribute.getName(), namespace));
-					resource.addProperty(property, attribute.getValue());
+//					Property property = model.createProperty(toIRI(attribute.getName(), namespace));
+//					resource.addProperty(property, attribute.getValue());
+					try {
+						builder.addValue(dataSourceId, resourceId, new URI(toIRI(attribute.getName(), namespace)), attribute.getValue());
+					} catch (URISyntaxException e) {
+						throw new IOException(e);
+					}
 				}
-				stack.add(resource);
+				stack.add(resourceId);
 			} else if (event.isCharacters()) {
 				// Characters
 				log.trace("character: {}", event);
@@ -172,9 +195,10 @@ public class XMLTriplifier implements Triplifier {
 				charBuilder.append(event.asCharacters().getData().trim());
 			}
 		}
-		DatasetGraph dg = DatasetFactory.create(model).asDatasetGraph();
-		dg.addGraph(NodeFactory.createURI(url.toString()), model.getGraph());
-		return dg;
+//		DatasetGraph dg = DatasetFactory.create(model).asDatasetGraph();
+//		dg.addGraph(NodeFactory.createURI(url.toString()), model.getGraph());
+//		return dg;
+		return builder.getDatasetGraph();
 	}
 
 	private String toIRI(QName qname, String namespace) {
