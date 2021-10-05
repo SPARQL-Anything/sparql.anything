bb8588f417c60152743dcdbf6aa569cfcdf8acdb
Luigi Asprino <luigi.asprino@gmail.com>
2021-10-05 16:02:23 +0200

    Experiments removed

--- experiment/run_cog_experiment.sh
+++
@@ -1,3 +0,0 @@
-mvn -f ../sparql.anything.experiment/pom.xml clean install
-THIS_FOLDER=$(pwd)
-mvn -f ../sparql.anything.experiment/pom.xml exec:java  -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="com.github.spiceh2020.sparql.anything.experiment.ComputeStats"  -Dexec.args="$THIS_FOLDER" 
