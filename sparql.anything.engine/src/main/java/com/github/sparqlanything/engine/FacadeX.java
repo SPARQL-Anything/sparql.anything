63bf947c8de4cf8eccaf90421c46968edd38661a
Luigi Asprino <luigi.asprino@gmail.com>
2021-10-07 16:50:52 +0200

    Digest utils md5hex function included

--- sparql.anything.engine/src/main/java/com/github/sparqlanything/engine/FacadeX.java
+++ sparql.anything.engine/src/main/java/com/github/sparqlanything/engine/FacadeX.java
@@ -23,6 +23,8 @@ package com.github.sparqlanything.engine;
 
 import com.github.sparqlanything.engine.functions.*;
 import com.github.sparqlanything.engine.functions.reflection.ReflectionFunctionFactory;
+
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.jena.query.ARQ;
 import org.apache.jena.sparql.engine.ExecutionContext;
 import org.apache.jena.sparql.engine.main.OpExecutor;
@@ -59,24 +61,24 @@ public final class FacadeX {
 			enablingMagicProperties();
 			enablingFunctions();
 			log.trace("Registering standard triplifiers");
-			Registry.registerTriplifier("com.github.sparqlanything.xml.XMLTriplifier",
-					new String[] { "xml" }, new String[] { "application/xml" });
-			Registry.registerTriplifier("com.github.sparqlanything.csv.CSVTriplifier",
-					new String[] { "csv" }, new String[] { "text/csv" });
-			Registry.registerTriplifier("com.github.sparqlanything.html.HTMLTriplifier",
-					new String[] { "html" }, new String[] { "text/html" });
-			Registry.registerTriplifier("com.github.sparqlanything.text.TextTriplifier",
-					new String[] { "txt" }, new String[] { "text/plain" });
-			Registry.registerTriplifier("com.github.sparqlanything.docs.DocxTriplifier",
-					new String[] { "docx" }, new String[] { "application/vnd.openxmlformats-officedocument.wordprocessingml.document" });
-			Registry.registerTriplifier("com.github.sparqlanything.zip.TarTriplifier",
-					new String[] { "tar" }, new String[] { "application/x-tar" });
-			Registry.registerTriplifier("com.github.sparqlanything.zip.ZipTriplifier",
-					new String[] { "zip" }, new String[] { "application/zip" });
+			Registry.registerTriplifier("com.github.sparqlanything.xml.XMLTriplifier", new String[] { "xml" },
+					new String[] { "application/xml" });
+			Registry.registerTriplifier("com.github.sparqlanything.csv.CSVTriplifier", new String[] { "csv" },
+					new String[] { "text/csv" });
+			Registry.registerTriplifier("com.github.sparqlanything.html.HTMLTriplifier", new String[] { "html" },
+					new String[] { "text/html" });
+			Registry.registerTriplifier("com.github.sparqlanything.text.TextTriplifier", new String[] { "txt" },
+					new String[] { "text/plain" });
+			Registry.registerTriplifier("com.github.sparqlanything.docs.DocxTriplifier", new String[] { "docx" },
+					new String[] { "application/vnd.openxmlformats-officedocument.wordprocessingml.document" });
+			Registry.registerTriplifier("com.github.sparqlanything.zip.TarTriplifier", new String[] { "tar" },
+					new String[] { "application/x-tar" });
+			Registry.registerTriplifier("com.github.sparqlanything.zip.ZipTriplifier", new String[] { "zip" },
+					new String[] { "application/zip" });
 			Registry.registerTriplifier("com.github.sparqlanything.binary.BinaryTriplifier",
 					new String[] { "bin", "dat" }, new String[] { "application/octet-stream" });
-			Registry.registerTriplifier("com.github.sparqlanything.json.JSONTriplifier",
-					new String[] { "json" }, new String[] { "application/json", "application/problem+json" });
+			Registry.registerTriplifier("com.github.sparqlanything.json.JSONTriplifier", new String[] { "json" },
+					new String[] { "application/json", "application/problem+json" });
 			Registry.registerTriplifier("com.github.sparqlanything.spreadsheet.SpreadsheetTriplifier",
 					new String[] { "xls", "xlsx" }, new String[] { "application/vnd.ms-excel",
 							"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" });
@@ -115,37 +117,39 @@ public final class FacadeX {
 	public static void enablingFunctions() {
 		log.trace("Enabling functions");
 		log.trace("Enabling collection functions");
-		FunctionRegistry.get().put(Triplifier.FACADE_X_CONST_NAMESPACE_IRI + "previous", Previous.class) ;
-		FunctionRegistry.get().put(Triplifier.FACADE_X_CONST_NAMESPACE_IRI + "next", Next.class) ;
-		FunctionRegistry.get().put(Triplifier.FACADE_X_CONST_NAMESPACE_IRI + "before", Before.class) ;
-		FunctionRegistry.get().put(Triplifier.FACADE_X_CONST_NAMESPACE_IRI + "after", After.class) ;
-		FunctionRegistry.get().put(Triplifier.FACADE_X_CONST_NAMESPACE_IRI + "backward", Backward.class) ;
-		FunctionRegistry.get().put(Triplifier.FACADE_X_CONST_NAMESPACE_IRI + "forward", Forward.class) ;
+		FunctionRegistry.get().put(Triplifier.FACADE_X_CONST_NAMESPACE_IRI + "previous", Previous.class);
+		FunctionRegistry.get().put(Triplifier.FACADE_X_CONST_NAMESPACE_IRI + "next", Next.class);
+		FunctionRegistry.get().put(Triplifier.FACADE_X_CONST_NAMESPACE_IRI + "before", Before.class);
+		FunctionRegistry.get().put(Triplifier.FACADE_X_CONST_NAMESPACE_IRI + "after", After.class);
+		FunctionRegistry.get().put(Triplifier.FACADE_X_CONST_NAMESPACE_IRI + "backward", Backward.class);
+		FunctionRegistry.get().put(Triplifier.FACADE_X_CONST_NAMESPACE_IRI + "forward", Forward.class);
 
 		log.trace("Enabling String functions");
 		FunctionRegistry.get().put(Triplifier.FACADE_X_CONST_NAMESPACE_IRI + "String.trim",
-				ReflectionFunctionFactory.get().makeFunction(String.class,"trim")) ;
+				ReflectionFunctionFactory.get().makeFunction(String.class, "trim"));
 		FunctionRegistry.get().put(Triplifier.FACADE_X_CONST_NAMESPACE_IRI + "String.substring",
-				ReflectionFunctionFactory.get().makeFunction(String.class,"substring")) ;
+				ReflectionFunctionFactory.get().makeFunction(String.class, "substring"));
 		FunctionRegistry.get().put(Triplifier.FACADE_X_CONST_NAMESPACE_IRI + "String.indexOf",
-				ReflectionFunctionFactory.get().makeFunction(String.class,"indexOf")) ;
+				ReflectionFunctionFactory.get().makeFunction(String.class, "indexOf"));
 		FunctionRegistry.get().put(Triplifier.FACADE_X_CONST_NAMESPACE_IRI + "String.startsWith",
-				ReflectionFunctionFactory.get().makeFunction(String.class,"startsWith")) ;
+				ReflectionFunctionFactory.get().makeFunction(String.class, "startsWith"));
 		FunctionRegistry.get().put(Triplifier.FACADE_X_CONST_NAMESPACE_IRI + "String.endsWith",
-				ReflectionFunctionFactory.get().makeFunction(String.class,"endsWith")) ;
+				ReflectionFunctionFactory.get().makeFunction(String.class, "endsWith"));
 		FunctionRegistry.get().put(Triplifier.FACADE_X_CONST_NAMESPACE_IRI + "String.endsWith",
-				ReflectionFunctionFactory.get().makeFunction(String.class,"endsWith")) ;
+				ReflectionFunctionFactory.get().makeFunction(String.class, "endsWith"));
+		FunctionRegistry.get().put(Triplifier.FACADE_X_CONST_NAMESPACE_IRI + "DigestUtils.md5Hex",
+				ReflectionFunctionFactory.get().makeFunction(DigestUtils.class, "md5Hex"));
 
 		try {
 			FunctionRegistry.get().put(Triplifier.FACADE_X_CONST_NAMESPACE_IRI + "String.toLowerCase",
-					ReflectionFunctionFactory.get().makeFunction(String.class.getMethod("toLowerCase") )) ;
+					ReflectionFunctionFactory.get().makeFunction(String.class.getMethod("toLowerCase")));
 			FunctionRegistry.get().put(Triplifier.FACADE_X_CONST_NAMESPACE_IRI + "String.toUpperCase",
-					ReflectionFunctionFactory.get().makeFunction(String.class.getMethod("toUpperCase") )) ;
+					ReflectionFunctionFactory.get().makeFunction(String.class.getMethod("toUpperCase")));
 		} catch (NoSuchMethodException e) {
 			throw new RuntimeException(e);
 		}
 
 		log.trace("Enabling function `serial`");
-		FunctionRegistry.get().put(Triplifier.FACADE_X_CONST_NAMESPACE_IRI + "serial", Serial.class) ;
+		FunctionRegistry.get().put(Triplifier.FACADE_X_CONST_NAMESPACE_IRI + "serial", Serial.class);
 	}
 }
