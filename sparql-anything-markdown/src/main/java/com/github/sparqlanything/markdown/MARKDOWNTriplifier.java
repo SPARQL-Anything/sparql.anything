08548759ba178513578ebed377c5a251868941e9
enridaga <enricodaga@gmail.com>
2021-10-21 14:44:23 +0100

    Initial tests for Document and Heading. See #125

--- sparql-anything-markdown/src/main/java/com/github/sparqlanything/markdown/MARKDOWNTriplifier.java
+++ sparql-anything-markdown/src/main/java/com/github/sparqlanything/markdown/MARKDOWNTriplifier.java
@@ -28,6 +28,8 @@ import com.github.sparqlanything.model.TriplifierHTTPException;
 import com.google.common.collect.Sets;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.jena.sparql.core.DatasetGraph;
+import org.apache.jena.vocabulary.RDF;
+import org.apache.jena.vocabulary.XSD;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -35,46 +37,46 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
-import java.util.Set;
+import java.util.*;
+
 import org.commonmark.node.*;
 import org.commonmark.parser.Parser;
-import org.commonmark.renderer.html.HtmlRenderer;
 
 public class MARKDOWNTriplifier extends AbstractVisitor implements Triplifier {
 
 	private final static Logger logger = LoggerFactory.getLogger(MARKDOWNTriplifier.class);
-
-	private String[] getDataSources(URL url) {
-		return new String[] { url.toString() };
-	}
-
-	private String getRootId(URL url, String dataSourceId, Properties properties) {
-		return Triplifier.getRootArgument(properties, url);
-	}
 
 	private URL url;
 	private Properties properties;
 	private FacadeXGraphBuilder builder;
 	private String dataSourceId;
 	private String rootId;
-	private List<String> containers;
+	private Map<Node, String> containers;
+	private Map<Class<? extends Node>, Integer> typeCounter;
+	private int lastLevel = 0;
+	private Map<String,Integer> lastSlot;
 
 	private void before(URL url, Properties properties, FacadeXGraphBuilder builder, String dataSourceId, String rootId){
 		this.url = url;
 		this.properties = properties;
 		this.builder = builder;
-		this.containers = new ArrayList<String>();
+		this.dataSourceId = dataSourceId;
+		this.rootId = rootId;
+		this.containers = new HashMap<Node,String>();
+		this.lastLevel = 0;
+		this.lastSlot = new HashMap<String,Integer>();
+		this.typeCounter = new HashMap<Class<? extends Node>,Integer>();
 	}
 
 	private void after(){
 		url = null;
 		properties = null;
-		builder = builder;
+		builder = null;
 		dataSourceId = null;
 		rootId = null;
+		this.containers = null;
+		this.lastLevel = 0;
+		this.lastSlot = null;
 	}
 
 	private void transform(URL url, Properties properties, FacadeXGraphBuilder builder)
@@ -87,18 +89,9 @@ public class MARKDOWNTriplifier extends AbstractVisitor implements Triplifier {
 
 		try {
 			// Only 1 data source expected
-			String dataSourceId;
-			if (properties.containsKey(IRIArgument.ROOT.toString())) {
-				logger.trace("Setting Data source Id using Root argument");
-				dataSourceId = properties.getProperty(IRIArgument.ROOT.toString());
-			} else if (properties.containsKey(IRIArgument.CONTENT.toString())) {
-				logger.trace("Setting Data source Id using Content argument");
-				dataSourceId = Triplifier.XYZ_NS
-						+ DigestUtils.md5Hex(properties.getProperty(IRIArgument.CONTENT.toString()));
-			} else {
-				dataSourceId = getDataSources(url)[0];
-			}
-
+			String dataSourceId = Triplifier.getRootArgument(properties, url);
+			rootId = dataSourceId;
+			logger.trace("ds {} root {}", dataSourceId, rootId);
 			before(url, properties, builder, dataSourceId, rootId);
 			document.accept(this);
 		} finally {
@@ -111,12 +104,7 @@ public class MARKDOWNTriplifier extends AbstractVisitor implements Triplifier {
 	public DatasetGraph triplify(Properties properties, FacadeXGraphBuilder builder)
 			throws IOException, TriplifierHTTPException {
 		URL url = Triplifier.getLocation(properties);
-
 		transform(url, properties, builder);
-
-//		if (logger.isDebugEnabled()) {
-//			logger.debug("Number of triples: {} ", builder.getMainGraph().size());
-//		}
 		return builder.getDatasetGraph();
 	}
 
@@ -130,41 +118,252 @@ public class MARKDOWNTriplifier extends AbstractVisitor implements Triplifier {
 		return Sets.newHashSet("md");
 	}
 
+
+	private void handleContainer(Node node){
+		String parentId = this.containers.get(node.getParent());
+		int slot = lastSlot.get(parentId) + 1;
+		if(!typeCounter.containsKey(node.getClass())){
+			typeCounter.put(node.getClass(), 0);
+		}
+		int counter = typeCounter.get(node.getClass()) + 1;
+		String containerId = String.join("/", parentId , node.getClass().getSimpleName().toLowerCase() , Integer.toString(counter));
+		this.typeCounter.put(node.getClass(), counter);
+		this.builder.addContainer(dataSourceId, parentId, slot, containerId);
+		this.builder.addType(dataSourceId, containerId, XYZ_NS + node.getClass().getSimpleName());
+		this.lastSlot.put(parentId, slot);
+		this.containers.put(node, containerId);
+		this.lastSlot.put(containerId, 0);
+	}
+
+	private void handleLiteral(Text node){
+		String parentId = this.containers.get(node.getParent());
+		int slot = lastSlot.get(parentId) + 1;
+		this.builder.addValue(dataSourceId,parentId, slot, node.getLiteral());
+		this.lastSlot.put(parentId, slot);
+	}
+
 	@Override
-	public void visit(Document document){
+	public void visit(Document node){
+		logger.trace("[Visiting {}] {}", node.getClass(), node);
+		logger.trace("[First child {}] {}", node.getFirstChild().getClass(), node.getFirstChild());
 		// Document is the root container
-		logger.trace("[Visiting {}] {}", document.getClass(), document);
-		this.visitChildren(document);
+		String containerId = rootId ;
+		this.builder.addType(dataSourceId, containerId, XYZ_NS + node.getClass().getSimpleName());
+//		this.lastSlot.put(parentId, slot);
+		this.containers.put(node, containerId);
+		this.lastSlot.put(containerId, 0);
+		this.builder.addRoot(dataSourceId, rootId);
+		this.visitChildren(node);
 	}
 
 	@Override
-	public void visit(Heading heading) {
-		logger.trace("[Visiting {}] {}", heading.getClass(), heading);
-		// Create container
-		super.visit(heading);
+	public void visit(Heading node) {
+		logger.trace("[Visiting {}] {}", node.getClass(), node);
+		logger.trace("[First child {}] {}", node.getFirstChild().getClass(), node.getFirstChild());
+		logger.trace("[Parent {}] {}", node.getParent().getClass(), node.getParent());
+
+		handleContainer(node);
+		// specify level
+		builder.addValue(dataSourceId, containers.get(node), "level", node.getLevel());
+		super.visit(node);
 	}
 
 	@Override
-	public void visit(Code code) {
-		logger.trace("[Visiting {}] {}", code.getClass(), code);
-		super.visit(code);
+	public void visit(Code node) {
+		logger.trace("[Visiting {}] {}", node.getClass(), node);
+		logger.trace("[First child {}] {}", node.getFirstChild().getClass(), node.getFirstChild());
+		logger.trace("[Parent {}] {}", node.getParent().getClass(), node.getParent());
+		handleContainer(node);
+		super.visit(node);
 	}
 
 	@Override
-	public void visit(ListItem listItem) {
-		logger.trace("[Visiting {}] {}", listItem.getClass(), listItem);
-		super.visit(listItem);
+	public void visit(ListItem node) {
+		logger.trace("[Visiting {}] {}", node.getClass(), node);
+		logger.trace("[First child {}] {}", node.getFirstChild().getClass(), node.getFirstChild());
+		logger.trace("[Parent {}] {}", node.getParent().getClass(), node.getParent());
+		handleContainer(node);
+		super.visit(node);
 	}
 
 	@Override
-	public void visit(OrderedList orderedList) {
-		logger.trace("[Visiting {}] {}", orderedList.getClass(), orderedList);
-		super.visit(orderedList);
+	public void visit(OrderedList node) {
+		logger.trace("[Visiting {}] {}", node.getClass(), node);
+		logger.trace("[First child {}] {}", node.getFirstChild().getClass(), node.getFirstChild());
+		logger.trace("[Parent {}] {}", node.getParent().getClass(), node.getParent());
+		handleContainer(node);
+		super.visit(node);
 	}
 
 	@Override
-	public void visit(Paragraph paragraph) {
-		logger.trace("[Visiting {}] {}", paragraph.getClass(), paragraph);
-		super.visit(paragraph);
+	public void visit(BulletList node) {
+		logger.trace("[Visiting {}] {}", node.getClass(), node);
+		logger.trace("[First child {}] {}", node.getFirstChild().getClass(), node.getFirstChild());
+		logger.trace("[Parent {}] {}", node.getParent().getClass(), node.getParent());
+		handleContainer(node);
+		super.visit(node);
+	}
+
+	@Override
+	public void visit(Paragraph node) {
+		logger.trace("[Visiting {}] {}", node.getClass(), node);
+		logger.trace("[First child {}] {}", node.getFirstChild().getClass(), node.getFirstChild());
+		logger.trace("[Parent {}] {}", node.getParent().getClass(), node.getParent());
+		logger.trace("[SourceSpans] {}", node.getSourceSpans());
+		handleContainer(node);
+		super.visit(node);
+	}
+
+	@Override
+	public void visit(Text node) {
+		logger.trace("[Visiting {}] {}", node.getClass(), node);
+		//logger.trace("[First child {}] {}", node.getFirstChild().getClass(), node.getFirstChild());
+		logger.trace("[Parent {}] {}", node.getParent().getClass(), node.getParent());
+		logger.trace("[Literal] {}", node.getLiteral());
+		logger.trace("[SourceSpans] {}", node.getSourceSpans());
+		handleLiteral(node);
+		super.visit(node);
+	}
+
+	@Override
+	public void visit(Link node) {
+		logger.trace("[Visiting {}] {}", node.getClass(), node);
+		logger.trace("[toString {}] {}", node.getClass(), node.toString());
+		//logger.trace("[First child {}] {}", node.getFirstChild().getClass(), node.getFirstChild());
+		logger.trace("[Parent {}] {}", node.getParent().getClass(), node.getParent());
+		handleContainer(node);
+		builder.addValue(dataSourceId, containers.get(node), "title", node.getTitle());
+		builder.addValue(dataSourceId, containers.get(node), "destination", node.getDestination());
+		super.visit(node);
+	}
+
+	@Override
+	public void visit(BlockQuote node) {
+		logger.trace("[Visiting {}] {}", node.getClass(), node);
+		logger.trace("[First child {}] {}", node.getFirstChild().getClass(), node.getFirstChild());
+		logger.trace("[Parent {}] {}", node.getParent().getClass(), node.getParent());
+		handleContainer(node);
+		super.visit(node);
+	}
+
+	@Override
+	public void visit(Emphasis node) {
+		logger.trace("[Visiting {}] {}", node.getClass(), node);
+//		logger.trace("[toString {}] {}", node.getClass(), node.toString());
+		//logger.trace("[First child {}] {}", node.getFirstChild().getClass(), node.getFirstChild());
+		logger.trace("[Parent {}] {}", node.getParent().getClass(), node.getParent());
+		handleContainer(node);
+		super.visit(node);
+	}
+
+	@Override
+	public void visit(FencedCodeBlock node) {
+		logger.trace("[Visiting {}] {}", node.getClass(), node);
+		logger.trace("[Parent {}] {}", node.getParent().getClass(), node.getParent());
+		handleContainer(node);
+		super.visit(node);
+	}
+
+	@Override
+	public void visit(HardLineBreak node) {
+		logger.trace("[Visiting {}] {}", node.getClass(), node);
+		logger.trace("[Parent {}] {}", node.getParent().getClass(), node.getParent());
+		handleContainer(node);
+		super.visit(node);
+	}
+
+	@Override
+	public void visit(ThematicBreak node) {
+		logger.trace("[Visiting {}] {}", node.getClass(), node);
+		logger.trace("[Parent {}] {}", node.getParent().getClass(), node.getParent());
+		handleContainer(node);
+		super.visit(node);
+	}
+
+	@Override
+	public void visit(HtmlInline node) {
+		logger.trace("[Visiting {}] {}", node.getClass(), node);
+		logger.trace("[Parent {}] {}", node.getParent().getClass(), node.getParent());
+		handleContainer(node);
+		super.visit(node);
+	}
+
+	@Override
+	public void visit(HtmlBlock node) {
+		logger.trace("[Visiting {}] {}", node.getClass(), node);
+		logger.trace("[Parent {}] {}", node.getParent().getClass(), node.getParent());
+		handleContainer(node);
+		super.visit(node);
+	}
+
+	@Override
+	public void visit(IndentedCodeBlock node) {
+		logger.trace("[Visiting {}] {}", node.getClass(), node);
+		logger.trace("[Parent {}] {}", node.getParent().getClass(), node.getParent());
+		handleContainer(node);
+		super.visit(node);
+	}
+
+	@Override
+	public void visit(Image node) {
+		logger.trace("[Visiting {}] {}", node.getClass(), node);
+		logger.trace("[Parent {}] {}", node.getParent().getClass(), node.getParent());
+		handleContainer(node);
+		super.visit(node);
+	}
+
+	@Override
+	public void visit(SoftLineBreak node) {
+		logger.trace("[Visiting {}] {}", node.getClass(), node);
+		logger.trace("[Parent {}] {}", node.getParent().getClass(), node.getParent());
+		handleContainer(node);
+		super.visit(node);
+	}
+
+	@Override
+	public void visit(LinkReferenceDefinition node) {
+		logger.trace("[Visiting {}] {}", node.getClass(), node);
+		logger.trace("[toString {}] {}", node.getClass(), node.toString());
+		//logger.trace("[First child {}] {}", node.getFirstChild().getClass(), node.getFirstChild());
+		logger.trace("[Parent {}] {}", node.getParent().getClass(), node.getParent());
+		handleContainer(node);
+		builder.addValue(dataSourceId, containers.get(node), "title", node.getTitle());
+		builder.addValue(dataSourceId, containers.get(node), "destination", node.getDestination());
+		builder.addValue(dataSourceId, containers.get(node), "label", node.getLabel());
+		super.visit(node);
+	}
+
+	@Override
+	public void visit(StrongEmphasis node) {
+		logger.trace("[Visiting {}] {}", node.getClass(), node);
+		logger.trace("[toString {}] {}", node.getClass(), node.toString());
+		//logger.trace("[First child {}] {}", node.getFirstChild().getClass(), node.getFirstChild());
+		logger.trace("[Parent {}] {}", node.getParent().getClass(), node.getParent());
+		handleContainer(node);
+		super.visit(node);
+	}
+
+	@Override
+	public void visit(CustomBlock node) {
+		logger.trace("[Visiting {}] {}", node.getClass(), node);
+		logger.trace("[Parent {}] {}", node.getParent().getClass(), node.getParent());
+		handleContainer(node);
+		super.visit(node);
+	}
+
+	@Override
+	public void visit(CustomNode node) {
+		logger.trace("[Visiting {}] {}", node.getClass(), node);
+		logger.trace("[Parent {}] {}", node.getParent().getClass(), node.getParent());
+		logger.trace("[ignoring]");
+//		String parentId = this.containers.get(node.getParent());
+//		String containerId = String.join("/",parentId , node.getClass().getSimpleName() , Integer.toString(node.hashCode())) ;
+//		handleContainer(node, containerId);
+		super.visit(node);
+	}
+
+	@Override
+	protected void visitChildren(Node parent) {
+		super.visitChildren(parent);
 	}
 }
--- sparql-anything-markdown/src/test/java/com/github/sparqlanything/markdown/MARKDOWNTriplifierTest.java
+++ sparql-anything-markdown/src/test/java/com/github/sparqlanything/markdown/MARKDOWNTriplifierTest.java
@@ -24,42 +24,172 @@ package com.github.sparqlanything.markdown;
 import com.github.sparqlanything.model.BaseFacadeXBuilder;
 import com.github.sparqlanything.model.Triplifier;
 import com.github.sparqlanything.model.TriplifierHTTPException;
-import org.apache.jena.graph.Node;
+import org.apache.jena.graph.Graph;
+
+import org.apache.jena.graph.NodeFactory;
 import org.apache.jena.graph.Triple;
-import org.apache.jena.sparql.core.DatasetGraph;
+import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.util.iterator.ExtendedIterator;
-import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.Iterator;
 import java.util.Properties;
+
+import static org.junit.Assert.assertTrue;
 
 public class MARKDOWNTriplifierTest {
 
 	private static final Logger logger = LoggerFactory.getLogger(MARKDOWNTriplifierTest.class);
 
-	@Test
-	public void test() throws TriplifierHTTPException, IOException, URISyntaxException {
+	@Rule
+	public TestName name = new TestName();
 
-		URL url = getClass().getClassLoader().getResource("./simple.md");
-		Triplifier t = new MARKDOWNTriplifier();
+//	@Test
+//	public void test() throws TriplifierHTTPException, IOException, URISyntaxException {
+//
+//		URL url = getClass().getClassLoader().getResource("./simple.md");
+//		Triplifier t = new MARKDOWNTriplifier();
+//		Properties properties = new Properties();
+//		properties.setProperty("location", url.toURI().toString());
+//		DatasetGraph ds = t.triplify(properties, new BaseFacadeXBuilder("simple.md", properties));
+//		ExtendedIterator<Triple> triples = ds.getDefaultGraph().find();
+//		while(triples.hasNext()){
+//			logger.trace("{}",triples.next());
+//		}
+//		Iterator<Node> graphs = ds.listGraphNodes();
+//		while(graphs.hasNext()){
+//			System.out.println(graphs.next());
+//		}
+////		Assert.assertTrue(ds.size() == 2);
+////		Assert.assertTrue(ds.getDefaultGraph().size() == 11);
+//	}
+
+	private Graph result;
+	private Graph expected;
+
+	@Before
+	public void before() throws TriplifierHTTPException, IOException, URISyntaxException {
+		// Root is Document
+		String fileName = name.getMethodName().substring(4) + ".md";
+		logger.debug("Input filename: {}", fileName);
+		URL url = getClass().getClassLoader().getResource(fileName);
 		Properties properties = new Properties();
 		properties.setProperty("location", url.toURI().toString());
-		DatasetGraph ds = t.triplify(properties, new BaseFacadeXBuilder("simple.md", properties));
-		ExtendedIterator<Triple> triples = ds.getDefaultGraph().find();
-		while(triples.hasNext()){
-			logger.trace("{}",triples.next());
+		properties.setProperty("blank-nodes", "false");
+		logger.debug("Input location: {}", url.toURI().toString());
+		properties.setProperty("root", "http://www.example.org/document");
+		Triplifier tr = new MARKDOWNTriplifier();
+		String graphName = Triplifier.getRootArgument(properties, url);
+		logger.debug("Graph name: {}", graphName);
+		this.result = tr.triplify(properties, new BaseFacadeXBuilder(graphName, properties)).getGraph(NodeFactory.createURI(graphName));
+
+		// RDF file name
+		String rdfFileName = name.getMethodName().substring(4) + ".ttl";
+		expected = RDFDataMgr.loadModel(getClass().getClassLoader().getResource(rdfFileName).toURI().toString()).getGraph();
+
+		if(logger.isWarnEnabled()) {
+			ExtendedIterator<Triple> it = expected.find();
+			while (it.hasNext()) {
+				Triple t = it.next();
+				logger.trace("E>> {}", t);
+
+				if(!result.contains(t)){
+					logger.warn("{} not found in result", t);
+					logger.warn("(T) {} {} {} {}", t.getSubject().getClass().getSimpleName(),  t.getPredicate().getClass().getSimpleName(), t.getObject().getClass().getSimpleName(), (t.getObject().isLiteral() && t.getObject().getLiteralDatatypeURI() != null) ? t.getObject().getLiteralDatatypeURI():null);
+				}
+			}
+			it = result.find();
+			while (it.hasNext()) {
+				Triple t = it.next();
+				logger.trace("<<R {}", t);
+				if(!expected.contains(t)){
+					logger.warn("{} not found in expected", t);
+					logger.warn("(T) {} {} {} {}", t.getSubject().getClass().getSimpleName(),  t.getPredicate().getClass().getSimpleName(), t.getObject().getClass().getSimpleName(), (t.getObject().isLiteral() && t.getObject().getLiteralDatatypeURI() != null) ? t.getObject().getLiteralDatatypeURI():null);
+				}
+			}
 		}
-		Iterator<Node> graphs = ds.listGraphNodes();
-		while(graphs.hasNext()){
-			System.out.println(graphs.next());
-		}
-//		Assert.assertTrue(ds.size() == 2);
-//		Assert.assertTrue(ds.getDefaultGraph().size() == 11);
 	}
+
+	@Test
+	public void testDocument() {
+		logger.debug("Test: {}", name.getMethodName());
+		assertTrue(this.result.isIsomorphicWith(expected));
+	}
+
+	@Test
+	public void testHeading(){
+		logger.debug("Test: {}", name.getMethodName());
+		assertTrue(this.result.isIsomorphicWith(expected));
+	}
+//
+//	@Test
+//	public void testCode(){}
+//
+//	@Test
+//	public void testListItem(){}
+//
+//	@Test
+//	public void testOrderedList(){}
+//
+//	@Test
+//	public void testBulletList(){}
+//
+//	@Test
+//	public void testParagraph(){}
+//
+//	@Test
+//	public void testText(){}
+//
+//	@Test
+//	public void testLink(){}
+//
+//	@Test
+//	public void testBlockQuote(){}
+//
+//	@Test
+//	public void testEmphasis(){}
+//
+//	@Test
+//	public void testFencedCodeBlock(){}
+//
+//	@Test
+//	public void testHardLineBreak(){}
+//
+//	@Test
+//	public void testThematicBreak(){}
+//
+//	@Test
+//	public void testHtmlInline(){}
+//
+//	@Test
+//	public void testHtmlBlock(){}
+//
+//	@Test
+//	public void testIndentedCodeBlock(){}
+//
+//	@Test
+//	public void testImage(){}
+//
+//	@Test
+//	public void testSoftLineBreak(){}
+//
+//	@Test
+//	public void testLinkReferenceDefinition(){}
+//
+//	@Test
+//	public void testStrongEmphasis(){}
+//
+//	@Test
+//	public void testCustomBlock(){}
+//
+//	@Test
+//	public void testCustomNode(){}
+
 }
