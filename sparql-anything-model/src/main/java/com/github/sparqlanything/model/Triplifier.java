SPARQL-Anything/sparql.anything

commit af8c496f63c7e82f4cf9d26f17374430553a75f4

Remove exception, about #186

sparql-anything-model/src/main/java/com/github/sparqlanything/model/Triplifier.java
@@ -222,17 +222,20 @@ public interface Triplifier {
 		return getInputStream(url, properties, getCharsetArgument(properties));
 	}
 	
-	public static String getResourceId(Properties properties) throws MalformedURLException {
-		String resourceId;
-		URL url = Triplifier.getLocation(properties);
-		if (url == null) {
+	public static String getResourceId(Properties properties) {
+		String resourceId = null;
+		URL url = null;
+		try {
+			url = Triplifier.getLocation(properties);
+		} catch (MalformedURLException e) {
+			log.error("Malformed url", e);
+		}
+		if (url == null && properties.containsKey(IRIArgument.CONTENT.toString())) {
 			// XXX This method of passing content seems only supported by the
 			// TextTriplifier.
 			log.trace("No location, use content: {}", properties.getProperty(IRIArgument.CONTENT.toString()));
 			String id = Integer.toString(properties.getProperty(IRIArgument.CONTENT.toString(), "").toString().hashCode());
 			resourceId = "content:" + id;
-		} else {
-			resourceId = url.toString();
 		}
 		return resourceId;
 	}

commit adff7fe837e034baee7e13b5fe8763743f2546d3

Fixing build broken by af8c496f63c7e82f4cf9d26f17374430553a75f4

sparql-anything-model/src/main/java/com/github/sparqlanything/model/Triplifier.java
@@ -245,7 +245,6 @@ public interface Triplifier {
 		URL url = null;
 		try {
 			url = Triplifier.getLocation(properties);
-			resourceId = url.toString();
 		} catch (MalformedURLException e) {
 			log.error("Malformed url", e);
 		}
@@ -255,6 +254,8 @@ public interface Triplifier {
 			log.trace("No location, use content: {}", properties.getProperty(IRIArgument.CONTENT.toString()));
 			String id = Integer.toString(properties.getProperty(IRIArgument.CONTENT.toString(), "").toString().hashCode());
 			resourceId = "content:" + id;
+		}else if(url != null){
+			resourceId = url.toString();
 		}
 		return resourceId;
 	}