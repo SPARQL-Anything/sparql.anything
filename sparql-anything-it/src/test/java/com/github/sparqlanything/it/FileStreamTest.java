1585b5129e1dd88c7a364029ff958001f58e6bde
Enrico Daga <enrico.daga@open.ac.uk>
2021-12-22 11:55:06 +0000

    Add tests to #189

--- sparql-anything-it/src/test/java/com/github/sparqlanything/it/FileStreamTest.java
+++ sparql-anything-it/src/test/java/com/github/sparqlanything/it/FileStreamTest.java
@@ -18,13 +18,16 @@
 package com.github.sparqlanything.it;
 
 import com.github.sparqlanything.csv.CSVTriplifier;
+import com.github.sparqlanything.engine.FacadeX;
 import com.github.sparqlanything.model.Triplifier;
 import com.github.sparqlanything.model.filestream.FileStreamDatasetGraph;
 import com.github.sparqlanything.model.filestream.FileStreamManager;
 import org.apache.jena.graph.Node;
 import org.apache.jena.graph.NodeFactory;
-import org.apache.jena.query.ARQ;
+import org.apache.jena.query.*;
 import org.apache.jena.sparql.core.Quad;
+import org.apache.jena.sparql.engine.main.QC;
+import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -42,7 +45,6 @@ public class FileStreamTest {
 		return NodeFactory.createVariable(v);
 	}
 
-	@Ignore
 	@Test
 	public void test() throws URISyntaxException, MalformedURLException {
 		Properties properties = new Properties();
@@ -50,8 +52,31 @@ public class FileStreamTest {
 		FileStreamManager man = new FileStreamManager(ARQ.getContext(), properties, new CSVTriplifier());
 		FileStreamDatasetGraph dg = new FileStreamDatasetGraph(man);
 		Iterator<Quad> it = dg.findNG(var("g"),var("s"),var("p"),var("o"));
+		int c=0;
 		while(it.hasNext()){
-			System.err.println(it.next());
+			c++;
 		}
+		Assert.assertTrue(c == 16);
+	}
+
+	@Ignore
+	@Test
+	public void testQueryJoin() throws URISyntaxException, MalformedURLException {
+		Dataset kb = DatasetFactory.createGeneral();
+		QC.setFactory(ARQ.getContext(), FacadeX.ExecutorFactory);
+		String location = getClass().getClassLoader().getResource("test1.csv").toURI().toString();
+		Query query = QueryFactory.create(
+				"PREFIX fx:  <http://sparql.xyz/facade-x/ns/>\n" +
+						"PREFIX xyz: <http://sparql.xyz/facade-x/data/>\n" +
+//						"SELECT * WHERE { SERVICE <x-sparql-anything:csv.headers=true,strategy=2,location="
+//						+ location + "> { ?a ?b ?c }} ");
+						"SELECT ?a ?b ?c ?d WHERE { SERVICE <x-sparql-anything:csv.headers=true,strategy=2,location="
+						+ location + "> { [] xyz:A ?a ; xyz:B ?b ; xyz:C ?c ; xyz:D ?d . filter(?d != \"\") }} ");
+		ResultSet rs = QueryExecutionFactory.create(query, kb).execSelect();
+		int c = 0;
+		while(rs.hasNext()){
+			c++;
+		}
+		Assert.assertTrue(c ==2);
 	}
 }
