7828965d866f9f1b2165eb238d3bdaf12830c060
Enrico Daga <enrico.daga@open.ac.uk>
2022-03-03 11:43:33 +0000

    Enable test on mac only. See #221

--- sparql-anything-it/src/test/java/com/github/sparqlanything/it/CommandTest.java
+++ sparql-anything-it/src/test/java/com/github/sparqlanything/it/CommandTest.java
@@ -26,22 +26,52 @@ import org.apache.jena.query.QueryFactory;
 import org.apache.jena.query.ResultSet;
 import org.apache.jena.sparql.core.DatasetGraphFactory;
 import org.apache.jena.sparql.engine.main.QC;
-import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestName;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 public class CommandTest {
+	private static final Logger log = LoggerFactory.getLogger(CommandTest.class);
 
-	@Ignore // Platform dependent!
+	// Operating systems
+	public enum OS {
+		WINDOWS, LINUX, MAC, SOLARIS
+	};
+
+	static {
+		String operSys = System.getProperty("os.name").toLowerCase();
+		if (operSys.contains("win")) {
+			platform = OS.WINDOWS;
+		} else if (operSys.contains("nix") || operSys.contains("nux")
+				|| operSys.contains("aix")) {
+			platform = OS.LINUX;
+		} else if (operSys.contains("mac")) {
+			platform = OS.MAC;
+		} else if (operSys.contains("sunos")) {
+			platform = OS.SOLARIS;
+		}
+	}
+	static OS platform;
+
+	@Rule
+	public TestName name = new TestName();
+
 	@Test
 	public void testEcho() throws IOException {
-		QC.setFactory(ARQ.getContext(), FacadeX.ExecutorFactory);
-		Query query = QueryFactory.create(IOUtils.toString(getClass().getClassLoader().getResourceAsStream("echo.sparql"), StandardCharsets.UTF_8));
-		ResultSet rs1 = QueryExecutionFactory.create(query, DatasetGraphFactory.create()).execSelect();
-		while(rs1.hasNext()){
-			System.err.println(rs1.next());
+		if(platform == OS.MAC) {
+			QC.setFactory(ARQ.getContext(), FacadeX.ExecutorFactory);
+			Query query = QueryFactory.create(IOUtils.toString(getClass().getClassLoader().getResourceAsStream("commands/echo.sparql"), StandardCharsets.UTF_8));
+			ResultSet rs1 = QueryExecutionFactory.create(query, DatasetGraphFactory.create()).execSelect();
+			while (rs1.hasNext()) {
+				System.err.println(rs1.next());
+			}
+		}else{
+			log.warn("Skipping test (platform not supported) {}", name.getMethodName());
 		}
 	}
 }
