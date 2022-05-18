8201d0d85045abd5f5ac3f2d2473343bfdb347a4
Enrico Daga <enrico.daga@open.ac.uk>
2022-05-18 17:38:07 +0100

    Update documentation script. See #201

--- update-docs.sh
+++ update-docs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 git add README.md .readthedocs.yaml mkdocs.yaml
-git add format/*
+git add formats/*
 cp -f README.md docs/
 cp -f BROWSER.md docs/
 cp -f LICENSE docs/
-cp -r format docs/
+cp -r formats docs/
 git add docs/*
 git commit -m "Update documentation #201 $1"
 git push
