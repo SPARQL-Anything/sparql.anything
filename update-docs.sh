d3113d986fb946e9082d4e112d2ee47ee2f6fd00
Luigi Asprino <luigi.asprino@gmail.com>
2022-08-03 18:23:40 +0200

    #201 update update-docs script

--- update-docs.sh
+++ update-docs.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-git add README.md .readthedocs.yaml mkdocs.yaml
+git add README.md .readthedocs.yaml mkdocs.yaml Configuration.md
 git add formats/*
 cp -f README.md docs/
 cp -f BROWSER.md docs/
+cp -f Configuration.md docs/
 cp -f LICENSE docs/
 cp -r formats docs/
 git add docs/*
