b27a6de94cf2176835b4a9c9229b4247fb862518
Luigi Asprino <luigi.asprino@gmail.com>
2022-08-10 16:28:03 +0200

    #201 update script

--- update-docs.sh
+++ update-docs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git add README.md .readthedocs.yaml mkdocs.yaml Configuration.md sparql-anything-it/src/test/java/com/github/sparqlanything/it/DocumentationExampleSandbox.java
+git add README.md .readthedocs.yaml mkdocs.yaml Configuration.md FUNCTIONS_AND_MAGIC_PROPERTIES.md sparql-anything-it/src/test/java/com/github/sparqlanything/it/DocumentationExampleSandbox.java
 git add formats/*
 cp -f README.md docs/
 cp -f BROWSER.md docs/
