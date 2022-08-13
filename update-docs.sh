59f6c93963c1727e368e5d46af7fc16f8d250c53
Luigi Asprino <luigi.asprino@gmail.com>
2022-08-13 08:04:15 +0200

    #201 including tutorials in the update-docs script

--- update-docs.sh
+++ update-docs.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-git add README.md .readthedocs.yaml mkdocs.yaml Configuration.md FUNCTIONS_AND_MAGIC_PROPERTIES.md sparql-anything-it/src/test/java/com/github/sparqlanything/it/DocumentationExampleSandbox.java
+git add README.md .readthedocs.yaml mkdocs.yaml Configuration.md TUTORIALS.md A_GENTLE_INTRODUCTION_TO_SPARQL_ANYTHING.md FUNCTIONS_AND_MAGIC_PROPERTIES.md sparql-anything-it/src/test/java/com/github/sparqlanything/it/DocumentationExampleSandbox.java
 git add formats/*
 cp -f README.md docs/
 cp -f BROWSER.md docs/
 cp -f Configuration.md docs/
 cp -f FUNCTIONS_AND_MAGIC_PROPERTIES.md docs/
+cp -f TUTORIALS.md docs/
+cp -f A_GENTLE_INTRODUCTION_TO_SPARQL_ANYTHING.md docs/
 cp -f LICENSE docs/
 cp -r formats docs/
 git add docs/*
