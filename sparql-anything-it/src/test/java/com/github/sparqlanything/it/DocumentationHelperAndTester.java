659195fc244bbacd3acd17a963309308dae79b5b
Luigi Asprino <luigi.asprino@gmail.com>
2022-08-18 16:35:46 +0200

    #201

--- sparql-anything-it/src/test/java/com/github/sparqlanything/it/DocumentationHelperAndTester.java
+++ sparql-anything-it/src/test/java/com/github/sparqlanything/it/DocumentationHelperAndTester.java
@@ -17,24 +17,65 @@
 package com.github.sparqlanything.it;
 
 import com.github.sparqlanything.engine.FacadeX;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.jena.query.*;
-import org.apache.jena.rdf.model.Model;
 import org.apache.jena.sparql.engine.main.QC;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
 
 public class DocumentationHelperAndTester {
 
-	public static void main(String[] args) {
+
+	public static void extractFunctionAndMagicProperties() throws IOException, URISyntaxException {
 		Dataset ds = DatasetFactory.createGeneral();
 		QC.setFactory(ARQ.getContext(), FacadeX.ExecutorFactory);
 
+		String queryFileName = "DocExamples/extract.sparql";
+		URI queryFile = DocumentationHelperAndTester.class.getClassLoader().getResource(queryFileName).toURI();
 		String location = "https://raw.githubusercontent.com/SPARQL-Anything/sparql.anything/v0.8-DEV/FUNCTIONS_AND_MAGIC_PROPERTIES.md";
-		String queryStringPattern = "PREFIX xyz: <http://sparql.xyz/facade-x/data/> PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> SELECT * { SERVICE<x-sparql-anything:%s> {?s a xyz:FencedCodeBlock ; rdf:_1 ?f }} ";
+		String queryStringPattern = IOUtils.toString(queryFile, StandardCharsets.UTF_8);
 		String queryString = String.format(queryStringPattern, location);
 
-		Query query = QueryFactory.create(queryString);
+		Query extractionQuery = QueryFactory.create(queryString);
+		QueryExecution qexec = QueryExecutionFactory.create(extractionQuery, ds);
 
-//		QueryExecution qexec = QueryExecutionFactory.create(query,ds);
-//		System.out.println(ResultSetFormatter.asText(qexec.execSelect()));
+		String outDirectoryPath = "FunctionsAndMagicProperties/";
+		new File(outDirectoryPath).mkdirs();
 
+		ResultSet rs = qexec.execSelect();
+		while (rs.hasNext()) {
+			QuerySolution qs = rs.next();
+			String query = qs.getLiteral("query").getValue().toString();
+			Query queryFunction = QueryFactory.create(query);
+			String result = qs.getLiteral("result").getValue().toString();
+			String functionName = qs.getLiteral("functionName").getValue().toString().replace(":", "").replace(".", "").toLowerCase();
+			System.out.println(functionName);
+			System.out.println(query);
+			if (queryFunction.isSelectType()) {
+				String actualResult = ResultSetFormatter.asText(QueryExecutionFactory.create(queryFunction, ds).execSelect());
+
+				if (!actualResult.equals(result)) {
+					System.err.println("Error");
+				}
+				System.out.println("Actual");
+				System.out.println(actualResult);
+
+			}
+			System.out.println("Expected");
+			System.out.println(result);
+			System.out.println(new File(outDirectoryPath + functionName + ".sparql"));
+			FileUtils.write(new File(outDirectoryPath + functionName + ".sparql"), query, "UTF-8");
+			FileUtils.write(new File(outDirectoryPath + functionName + ".csv"), result, "UTF-8");
+		}
 	}
+
+	public static void main(String[] args) throws IOException, URISyntaxException {
+		extractFunctionAndMagicProperties();
+	}
+
 }
