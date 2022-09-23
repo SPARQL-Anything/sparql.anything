f72e32b734ec3bebfad89367b4cfc3581f565b28
Luigi Asprino <luigi.asprino@gmail.com>
2022-09-23 13:13:32 +0200

    Add tests #308

--- sparql-anything-spreadsheet/src/test/java/com/github/sparqlanything/spreadsheet/test/TestTriplifier.java
+++ sparql-anything-spreadsheet/src/test/java/com/github/sparqlanything/spreadsheet/test/TestTriplifier.java
@@ -25,6 +25,10 @@ import org.apache.jena.graph.Graph;
 import org.apache.jena.graph.Node;
 import org.apache.jena.graph.NodeFactory;
 import org.apache.jena.graph.Triple;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.sparql.core.DatasetGraph;
 import org.apache.jena.sparql.graph.GraphFactory;
 import org.apache.jena.vocabulary.RDF;
@@ -34,7 +38,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.Properties;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class TestTriplifier {
 
@@ -50,7 +54,7 @@ public class TestTriplifier {
 			st.triplify(p, builder);
 			dg = builder.getDatasetGraph();
 
-			String root = spreadsheet.toString() + "#";
+			String root = spreadsheet + "#";
 
 			Graph gs1 = GraphFactory.createGraphMem();
 			Node s1Root = NodeFactory.createBlankNode();
@@ -128,10 +132,10 @@ public class TestTriplifier {
 			st.triplify(p, builder);
 			dg = builder.getDatasetGraph();
 			dg.find(null, null, null, null).forEachRemaining(q -> {
-				assertTrue(!q.getSubject().isBlank());
-				assertTrue(!q.getPredicate().isBlank());
-				assertTrue(!q.getObject().isBlank());
-				assertTrue(!q.getGraph().isBlank());
+				assertFalse(q.getSubject().isBlank());
+				assertFalse(q.getPredicate().isBlank());
+				assertFalse(q.getObject().isBlank());
+				assertFalse(q.getGraph().isBlank());
 			});
 
 		} catch (IOException e) {
@@ -154,8 +158,7 @@ public class TestTriplifier {
 			dg = builder.getDatasetGraph();
 
 			dg.find(null, null, null, null).forEachRemaining(q -> {
-				boolean condition = q.getPredicate().getURI().startsWith(namespace)
-						|| q.getPredicate().getURI().startsWith(RDF.getURI());
+				boolean condition = q.getPredicate().getURI().startsWith(namespace) || q.getPredicate().getURI().startsWith(RDF.getURI());
 				assertTrue(condition);
 			});
 
@@ -177,7 +180,7 @@ public class TestTriplifier {
 			st.triplify(p, builder);
 			dg = builder.getDatasetGraph();
 
-			String root = spreadsheet.toString() + "#";
+			String root = spreadsheet + "#";
 
 			Graph gs1 = GraphFactory.createGraphMem();
 			Node s1Root = NodeFactory.createBlankNode();
@@ -185,21 +188,15 @@ public class TestTriplifier {
 
 			Node s1Row2 = NodeFactory.createBlankNode();
 			gs1.add(new Triple(s1Root, RDF.li(1).asNode(), s1Row2));
-			gs1.add(new Triple(s1Row2, NodeFactory.createURI(Triplifier.XYZ_NS + "A"),
-					NodeFactory.createLiteral("A1")));
-			gs1.add(new Triple(s1Row2, NodeFactory.createURI(Triplifier.XYZ_NS + "B"),
-					NodeFactory.createLiteral("B1")));
-			gs1.add(new Triple(s1Row2, NodeFactory.createURI(Triplifier.XYZ_NS + "C"),
-					NodeFactory.createLiteral("C1")));
+			gs1.add(new Triple(s1Row2, NodeFactory.createURI(Triplifier.XYZ_NS + "A"), NodeFactory.createLiteral("A1")));
+			gs1.add(new Triple(s1Row2, NodeFactory.createURI(Triplifier.XYZ_NS + "B"), NodeFactory.createLiteral("B1")));
+			gs1.add(new Triple(s1Row2, NodeFactory.createURI(Triplifier.XYZ_NS + "C"), NodeFactory.createLiteral("C1")));
 
 			Node s1Row3 = NodeFactory.createBlankNode();
 			gs1.add(new Triple(s1Root, RDF.li(2).asNode(), s1Row3));
-			gs1.add(new Triple(s1Row3, NodeFactory.createURI(Triplifier.XYZ_NS + "A"),
-					NodeFactory.createLiteral("A2")));
-			gs1.add(new Triple(s1Row3, NodeFactory.createURI(Triplifier.XYZ_NS + "B"),
-					NodeFactory.createLiteral("B2")));
-			gs1.add(new Triple(s1Row3, NodeFactory.createURI(Triplifier.XYZ_NS + "C"),
-					NodeFactory.createLiteral("C2")));
+			gs1.add(new Triple(s1Row3, NodeFactory.createURI(Triplifier.XYZ_NS + "A"), NodeFactory.createLiteral("A2")));
+			gs1.add(new Triple(s1Row3, NodeFactory.createURI(Triplifier.XYZ_NS + "B"), NodeFactory.createLiteral("B2")));
+			gs1.add(new Triple(s1Row3, NodeFactory.createURI(Triplifier.XYZ_NS + "C"), NodeFactory.createLiteral("C2")));
 
 			Graph gs2 = GraphFactory.createGraphMem();
 			Node s2Root = NodeFactory.createBlankNode();
@@ -207,21 +204,15 @@ public class TestTriplifier {
 
 			Node s2Row2 = NodeFactory.createBlankNode();
 			gs2.add(new Triple(s2Root, RDF.li(1).asNode(), s2Row2));
-			gs2.add(new Triple(s2Row2, NodeFactory.createURI(Triplifier.XYZ_NS + "A1"),
-					NodeFactory.createLiteral("A11")));
-			gs2.add(new Triple(s2Row2, NodeFactory.createURI(Triplifier.XYZ_NS + "B1"),
-					NodeFactory.createLiteral("B11")));
-			gs2.add(new Triple(s2Row2, NodeFactory.createURI(Triplifier.XYZ_NS + "C1"),
-					NodeFactory.createLiteral("C11")));
+			gs2.add(new Triple(s2Row2, NodeFactory.createURI(Triplifier.XYZ_NS + "A1"), NodeFactory.createLiteral("A11")));
+			gs2.add(new Triple(s2Row2, NodeFactory.createURI(Triplifier.XYZ_NS + "B1"), NodeFactory.createLiteral("B11")));
+			gs2.add(new Triple(s2Row2, NodeFactory.createURI(Triplifier.XYZ_NS + "C1"), NodeFactory.createLiteral("C11")));
 
 			Node s2Row3 = NodeFactory.createBlankNode();
 			gs2.add(new Triple(s2Root, RDF.li(2).asNode(), s2Row3));
-			gs2.add(new Triple(s2Row3, NodeFactory.createURI(Triplifier.XYZ_NS + "A1"),
-					NodeFactory.createLiteral("A12")));
-			gs2.add(new Triple(s2Row3, NodeFactory.createURI(Triplifier.XYZ_NS + "B1"),
-					NodeFactory.createLiteral("B12")));
-			gs2.add(new Triple(s2Row3, NodeFactory.createURI(Triplifier.XYZ_NS + "C1"),
-					NodeFactory.createLiteral("C12")));
+			gs2.add(new Triple(s2Row3, NodeFactory.createURI(Triplifier.XYZ_NS + "A1"), NodeFactory.createLiteral("A12")));
+			gs2.add(new Triple(s2Row3, NodeFactory.createURI(Triplifier.XYZ_NS + "B1"), NodeFactory.createLiteral("B12")));
+			gs2.add(new Triple(s2Row3, NodeFactory.createURI(Triplifier.XYZ_NS + "C1"), NodeFactory.createLiteral("C12")));
 
 //			RDFDataMgr.write(System.out, dg, RDFFormat.NQ);
 //			ModelFactory.createModelForGraph(gs1).write(System.out, "TTL");
@@ -256,7 +247,7 @@ public class TestTriplifier {
 			st.triplify(p, builder);
 			dg = builder.getDatasetGraph();
 
-			String root = spreadsheet.toString() + "#";
+			String root = spreadsheet + "#";
 
 			Graph gs1 = GraphFactory.createGraphMem();
 			Node s1Root = NodeFactory.createBlankNode();
@@ -320,7 +311,7 @@ public class TestTriplifier {
 
 
 	@Test
-	public void testCellLink (){
+	public void testCellLink() {
 		SpreadsheetTriplifier st = new SpreadsheetTriplifier();
 		URL spreadsheet = st.getClass().getClassLoader().getResource("./testResources/Book2.xlsx");
 		Properties p = new Properties();
@@ -333,6 +324,21 @@ public class TestTriplifier {
 			FacadeXGraphBuilder builder = new BaseFacadeXGraphBuilder(spreadsheet.toString(), p);
 			st.triplify(p, builder);
 			dg = builder.getDatasetGraph();
+
+//			RDFDataMgr.write(System.out, dg, Lang.TRIG);
+
+			String prefixes = "PREFIX xyz: <http://sparql.xyz/facade-x/data/> PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> ";
+			String[] asks = new String[]{" ASK {[ a xyz:STRING ; rdf:_1 \"C12\" ; xyz:address \"http://www.example.org/C12\" ; xyz:label \"C12\" ]}", " ASK {[ a xyz:STRING ; rdf:_1 \"B\" ]}", " ASK {[ a xyz:STRING ; rdf:_1 \"B12\" ; xyz:author ?author ; xyz:threadedComment ?tc  ] FILTER(REGEX(?tc, \"This is a comment\"))  }",};
+
+			for (String ask : asks) {
+				try {
+					assertTrue(QueryExecutionFactory.create(QueryFactory.create(prefixes + ask), dg).execAsk());
+				} catch (Exception e) {
+					fail(prefixes + ask);
+					fail(e.getMessage());
+				}
+			}
+
 
 		} catch (IOException e) {
 			e.printStackTrace();
