401e44c364b88663f69ae85b183bb2420cd20029
Enrico Daga <enrico.daga@open.ac.uk>
2022-12-16 16:04:27 +0000

    Reproduce #320

--- sparql-anything-cli/src/test/java/com/github/sparqlanything/cli/TestIssue320.java
+++ sparql-anything-cli/src/test/java/com/github/sparqlanything/cli/TestIssue320.java
@@ -5,13 +5,14 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 
 package com.github.sparqlanything.cli;
@@ -21,7 +22,7 @@ import io.github.basilapi.basil.sparql.Specification;
 import io.github.basilapi.basil.sparql.SpecificationFactory;
 import io.github.basilapi.basil.sparql.UnknownQueryTypeException;
 import org.apache.commons.io.IOUtils;
-import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSetFormatter;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -29,20 +30,20 @@ import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 import java.util.Iterator;
 
-public class TestParameterSubstitution {
+public class TestIssue320 {
+
 	@Test
-	public void test() throws IOException, UnknownQueryTypeException {
-		String str = IOUtils.toString(getClass().getClassLoader().getResourceAsStream("./queryWithParameter.sparql"), StandardCharsets.UTF_8);
-		Specification specification = SpecificationFactory.create("", str);
-		Collection<QueryParameter> co = specification.getParameters();
-		Iterator<QueryParameter> it = co.iterator();
-		while(it.hasNext()){
-			System.out.println(it.next().getName());
-		}
+	public void testIssue320() throws Exception {
+		// This is to reproduce Issue #320
+		ResultSetFormatter.outputAsCSV(System.err,true);
+		ResultSetFormatter.outputAsCSV(System.err,false);
+
+		String str = IOUtils.toString(getClass().getClassLoader().getResourceAsStream("./ask.sparql"), StandardCharsets.UTF_8);
+		String output = SPARQLAnything.callMain(new String[]{"-q", str, "-f", "text"});
+		System.out.println(output);
 //
 //		while (it.hasNext()) {
 //			QuerySolution qs = parameters.nextSolution();
 //		}
 	}
-
 }
