ee5f9ca06b5f5068df0093af6ca603c90778a57e
Luigi Asprino <luigi.asprino@gmail.com>
2023-06-09 10:55:32 +0200

    Update documentation #201 see #378 #377

---
+++ sparql-anything-engine/src/main/java/io/github/sparqlanything/engine/IsContainerMembershipProperty.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2023 SPARQL Anything Contributors @ http://github.com/sparql-anything
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.sparqlanything.engine;
+
+import io.github.sparqlanything.engine.functions.FXFunction;
+import org.apache.jena.sparql.expr.ExprEvalException;
+import org.apache.jena.sparql.expr.NodeValue;
+import org.apache.jena.sparql.function.FunctionBase1;
+import org.apache.jena.sparql.util.FmtUtils;
+
+public class IsContainerMembershipProperty extends FunctionBase1 implements FXFunction {
+
+	@Override
+	public NodeValue exec(NodeValue nodeValue) {
+		if(isContainerMembershipProperty(nodeValue)){
+			return NodeValue.TRUE;
+		}
+		return NodeValue.FALSE;
+	}
+}
