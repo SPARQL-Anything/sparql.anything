7d623c3740ed10295b0d9f8de6d79bbf40e269ce
Luigi Asprino <luigi.asprino@gmail.com>
2023-06-29 11:15:19 +0200

    Add test for #150

--- sparql-anything-cli/src/test/java/io/github/sparqlanything/cli/OptionsViaCommandLineTest.java
+++ sparql-anything-cli/src/test/java/io/github/sparqlanything/cli/OptionsViaCommandLineTest.java
@@ -1,13 +1,14 @@
 package io.github.sparqlanything.cli;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class OptionsViaCommandLineTest {
 
 	@Test
 	public void test() throws Exception {
-		String q = "SELECT * { SERVICE <x-sparql-anything:> { ?s ?p ?o } }";
+		String q = "SELECT ?abc { SERVICE <x-sparql-anything:> { ?root a <http://sparql.xyz/facade-x/ns/root> ;  <http://www.w3.org/1999/02/22-rdf-syntax-ns#_1> ?abc } }";
 		String out = SPARQLAnything.callMain(new String[]{"-q", q, "-c", "content=abc"});
-		System.out.println(out);
+		Assert.assertTrue(out.contains("abc"));
 	}
 }
