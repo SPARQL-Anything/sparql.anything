39ae05fd32fc58f03ccb42f70e05cd796e1148e9
Luigi Asprino <luigi.asprino@gmail.com>
2023-10-12 17:32:19 +0200

    Delegate FXWorkers and moves code in execute() methods of the FXOpExecutor to FXWorkers (see #150)

--- sparql-anything-engine/src/main/java/io/github/sparqlanything/engine/PropertyExtractor.java
+++ sparql-anything-engine/src/main/java/io/github/sparqlanything/engine/PropertyExtractor.java
@@ -17,16 +17,12 @@
 package io.github.sparqlanything.engine;
 
 import io.github.sparqlanything.engine.functions.IsFacadeXExtension;
-import io.github.sparqlanything.facadeiri.FacadeIRIParser;
 import io.github.sparqlanything.model.IRIArgument;
 import io.github.sparqlanything.model.Triplifier;
 import io.github.sparqlanything.zip.FolderTriplifier;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.jena.graph.Triple;
-import org.apache.jena.sparql.algebra.Op;
 import org.apache.jena.sparql.algebra.op.OpBGP;
-import org.apache.jena.sparql.algebra.op.OpService;
-import org.apache.jena.sparql.core.Var;
 import org.apache.jena.sparql.engine.ExecutionContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -100,7 +96,7 @@ class PropertyExtractor {
 		return t;
 	}
 
-	private static void extractPropertiesFromBGP(Properties properties, OpBGP bgp) throws UnboundVariableException {
+	 static void extractPropertiesFromBGP(Properties properties, OpBGP bgp) throws UnboundVariableException {
 		for (Triple t : bgp.getPattern().getList()) {
 			if (t.getSubject().isURI() && t.getSubject().getURI().equals(Triplifier.FACADE_X_TYPE_PROPERTIES)) {
 				if (t.getObject().isURI()) {
@@ -115,67 +111,67 @@ class PropertyExtractor {
 	}
 
 
-	static void extractPropertiesFromOp(Op op, Properties properties) throws UnboundVariableException {
-		if (op instanceof OpBGP) {
-			extractPropertiesFromBGP(properties, (OpBGP) op);
-		} else if (op instanceof OpService) {
-			OpService opService = (OpService) op;
-			String url = opService.getService().getURI();
-			// Parse IRI only if contains properties
-			if (!url.equals(FacadeIRIParser.SPARQL_ANYTHING_URI_SCHEMA)) {
-				FacadeIRIParser p = new FacadeIRIParser(url);
-				properties.putAll(p.getProperties());
-			}
-
-			// Setting defaults
-			if (!properties.containsKey(IRIArgument.NAMESPACE.toString())) {
-				logger.trace("Setting default value for namespace: {}", Triplifier.XYZ_NS);
-				properties.setProperty(IRIArgument.NAMESPACE.toString(), Triplifier.XYZ_NS);
-			}
-			// Setting silent
-			if (opService.getSilent()) {
-				// we can only see if silent was specified at the OpService so we need to stash
-				// a boolean
-				// at this point so we can use it when we triplify further down the Op tree
-				properties.setProperty(IRIArgument.OP_SERVICE_SILENT.toString(), "true");
-			}
-
-			Op next = opService.getSubOp();
-			FXBGPFinder vis = new FXBGPFinder();
-			next.visit(vis);
-			logger.trace("Has Table {}", vis.hasTable());
-
-			if (vis.getBGP() != null) {
-				try {
-					extractPropertiesFromBGP(properties, vis.getBGP());
-				} catch (UnboundVariableException e) {
-					if (vis.hasTable()) {
-						logger.trace(vis.getOpTable().toString());
-						logger.trace("BGP {}", vis.getBGP());
-						logger.trace("Contains variable names {}", vis.getOpTable().getTable().getVarNames().contains(e.getVariableName()));
-						if (vis.getOpTable().getTable().getVarNames().contains(e.getVariableName())) {
-							e.setOpTable(vis.getOpTable());
-						}
-					}
-
-					if (vis.getOpExtend() != null) {
-						logger.trace("OpExtend {}", vis.getOpExtend());
-						for (Var var : vis.getOpExtend().getVarExprList().getVars()) {
-							if (var.getName().equals(e.getVariableName())) {
-								e.setOpExtend(vis.getOpExtend());
-							}
-						}
-					}
-
-					throw e;
-				}
-				logger.trace("Number of properties {}: {}", properties.size(), properties.toString());
-			} else {
-				logger.trace("Couldn't find OpGraph");
-			}
-		}
-
-	}
+//	static void extractPropertiesFromOp(Op op, Properties properties) throws UnboundVariableException {
+//		if (op instanceof OpBGP) {
+//			extractPropertiesFromBGP(properties, (OpBGP) op);
+//		} else if (op instanceof OpService) {
+//			OpService opService = (OpService) op;
+//			String url = opService.getService().getURI();
+//			// Parse IRI only if contains properties
+//			if (!url.equals(FacadeIRIParser.SPARQL_ANYTHING_URI_SCHEMA)) {
+//				FacadeIRIParser p = new FacadeIRIParser(url);
+//				properties.putAll(p.getProperties());
+//			}
+//
+//			// Setting defaults
+//			if (!properties.containsKey(IRIArgument.NAMESPACE.toString())) {
+//				logger.trace("Setting default value for namespace: {}", Triplifier.XYZ_NS);
+//				properties.setProperty(IRIArgument.NAMESPACE.toString(), Triplifier.XYZ_NS);
+//			}
+//			// Setting silent
+//			if (opService.getSilent()) {
+//				// we can only see if silent was specified at the OpService so we need to stash
+//				// a boolean
+//				// at this point so we can use it when we triplify further down the Op tree
+//				properties.setProperty(IRIArgument.OP_SERVICE_SILENT.toString(), "true");
+//			}
+//
+//			Op next = opService.getSubOp();
+//			FXBGPFinder vis = new FXBGPFinder();
+//			next.visit(vis);
+//			logger.trace("Has Table {}", vis.hasTable());
+//
+//			if (vis.getBGP() != null) {
+//				try {
+//					extractPropertiesFromBGP(properties, vis.getBGP());
+//				} catch (UnboundVariableException e) {
+//					if (vis.hasTable()) {
+//						logger.trace(vis.getOpTable().toString());
+//						logger.trace("BGP {}", vis.getBGP());
+//						logger.trace("Contains variable names {}", vis.getOpTable().getTable().getVarNames().contains(e.getVariableName()));
+//						if (vis.getOpTable().getTable().getVarNames().contains(e.getVariableName())) {
+//							e.setOpTable(vis.getOpTable());
+//						}
+//					}
+//
+//					if (vis.getOpExtend() != null) {
+//						logger.trace("OpExtend {}", vis.getOpExtend());
+//						for (Var var : vis.getOpExtend().getVarExprList().getVars()) {
+//							if (var.getName().equals(e.getVariableName())) {
+//								e.setOpExtend(vis.getOpExtend());
+//							}
+//						}
+//					}
+//
+//					throw e;
+//				}
+//				logger.trace("Number of properties {}: {}", properties.size(), properties.toString());
+//			} else {
+//				logger.trace("Couldn't find OpGraph");
+//			}
+//		}
+//
+//	}
 
 
 	public static void extractPropertiesFromExecutionContext(ExecutionContext execCxt, Properties p) {
