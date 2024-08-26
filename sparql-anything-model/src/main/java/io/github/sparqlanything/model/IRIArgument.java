2b6108c2f76a25b576a183307a9c09e0957afb32
Luigi Asprino <luigi.asprino@gmail.com>
2024-08-26 17:18:28 +0200

    #331 Fix option name

--- sparql-anything-model/src/main/java/io/github/sparqlanything/model/IRIArgument.java
+++ sparql-anything-model/src/main/java/io/github/sparqlanything/model/IRIArgument.java
@@ -40,7 +40,7 @@ public class IRIArgument {
 	public static final IRIArgument ANNOTATE_TRIPLES_WITH_SLOT_KEYS = new IRIArgument("annotate-triples-with-slot-keys", "false");
 
 	public static final IRIArgument GENERATE_PREDICATE_LABELS = new IRIArgument("generate-predicate-labels", "false");
-    public static final IRIArgument  READ_FROM_STD_IN =  new IRIArgument(" ", "false");
+    public static final IRIArgument  READ_FROM_STD_IN =  new IRIArgument("read-from-std-in", "false");
 
     private final String name;
 	private final String defaultValue;
