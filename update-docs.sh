1b6e2209e9e7619407203c3d54acfa1a42a22415
Luigi Asprino <luigi.asprino@gmail.com>
2024-09-11 16:41:03 +0200

    #201 update documentation script

--- update-docs.sh
+++ update-docs.sh
@@ -24,6 +24,7 @@ cd ..
 
 git add README.md SystemOverview.md CLI.md EXTENSION_MECHANISMS.md JAVA_LIBRARY.md Facade-X.md facade-x-ER.png .readthedocs.yaml mkdocs.yaml Configuration.md TUTORIALS.md A_GENTLE_INTRODUCTION_TO_SPARQL_ANYTHING.md FUNCTIONS_AND_MAGIC_PROPERTIES.md sparql-anything-it/src/test/java/io/github/sparqlanything/it/DocumentationExampleSandbox.java
 git add formats/*
+git add imgs/*
 cp -f README.md docs/
 cp -f SystemOverview.md docs/
 cp -f EXTENSION_MECHANISMS.md docs/
@@ -39,6 +40,7 @@ cp -f TUTORIALS.md docs/
 cp -f A_GENTLE_INTRODUCTION_TO_SPARQL_ANYTHING.md docs/
 cp -f LICENSE docs/
 cp -r formats docs/
+cp -r imgs docs/
 git add docs/*
 git commit -m "#201 Update documentation '$1'"
 git push
