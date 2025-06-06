181b20367333eb42913b9b69add800e606a27d44
Enrico Daga <enrico.daga@open.ac.uk>
2025-06-06 16:43:27 +0100

    Fix pom version and cleanup

--- sparql-anything-jdbc/src/test/java/io/github/sparqlanything/fxbgp/AnalyserTest.java
+++ sparql-anything-jdbc/src/test/java/io/github/sparqlanything/fxbgp/AnalyserTest.java
@@ -96,7 +96,6 @@ public class AnalyserTest extends BGPTestAbstract {
 	@Test
 	public void NS6() throws IOException {
 		readBGP(name.getMethodName());
-		System.out.println(interpretations());
 		Assert.assertEquals(0, interpretations().size());
 	}
 
