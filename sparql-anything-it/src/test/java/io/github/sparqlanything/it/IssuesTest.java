66d20cebc60c9ec286212eed909c1bd0134c2e2d
Luigi Asprino <luigi.asprino@gmail.com>
2025-11-03 15:22:40 +0100

    #553 add test query

--- sparql-anything-it/src/test/java/io/github/sparqlanything/it/IssuesTest.java
+++ sparql-anything-it/src/test/java/io/github/sparqlanything/it/IssuesTest.java
@@ -40,8 +40,7 @@ import org.junit.Assume;
 import io.github.sparqlanything.model.HTTPHelper;
 
 import javax.xml.bind.DatatypeConverter;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
@@ -989,7 +988,7 @@ public class IssuesTest {
 	public void testIssue565() throws URISyntaxException, IOException {
 		String currentUserHome = System.getProperty("user.home");
 		System.setProperty("user.home","issues");
-		QueryExecution qExec = executeTest("issues/issue565.sparql", "issues/issue565.txt", false, false, true, false);
+		QueryExecution qExec = executeTest("issues/issue565.sparql", "issues/issue565.txt", false, false, false, false);
 		ResultSet rs = qExec.execSelect();
 		assertTrue(rs.hasNext());
 		System.setProperty("user.home", currentUserHome);
