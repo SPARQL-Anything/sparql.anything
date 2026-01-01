c263d82df4213833970763a328c245c55743d5d7
Luigi Asprino <luigi.asprino@gmail.com>
2026-01-01 08:39:54 +0100

    #600

--- sparql-anything-html/src/main/java/io/github/sparqlanything/html/org/apache/any23/extractor/package-info.java
+++ sparql-anything-html/src/main/java/io/github/sparqlanything/html/org/apache/any23/extractor/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 SPARQL Anything Contributors @ http://github.com/sparql-anything
+ * Copyright (c) 2026 SPARQL Anything Contributors @ http://github.com/sparql-anything
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,8 +14,7 @@
  * limitations under the License.
  */
 
-/*
- */
+
 
 /**
  * This package contains classes and interfaces modeling the {@link io.github.sparqlanything.html.org.apache.any23.extractor.Extractor} <i>API</i>.
--- sparql-anything-html/src/main/java/io/github/sparqlanything/html/org/semarglproject/sink/QuadSink.java
+++ sparql-anything-html/src/main/java/io/github/sparqlanything/html/org/semarglproject/sink/QuadSink.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 SPARQL Anything Contributors @ http://github.com/sparql-anything
+ * Copyright (c) 2026 SPARQL Anything Contributors @ http://github.com/sparql-anything
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,8 +14,7 @@
  * limitations under the License.
  */
 
-/*
- */
+
 
 package io.github.sparqlanything.html.org.semarglproject.sink;
 
